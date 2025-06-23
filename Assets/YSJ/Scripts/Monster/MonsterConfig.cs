b529b13107cfb2c418c2dd1c97b09247284d503c
DevIRU <deviru0413@gmail.com>
2025-06-24 02:52:04 +0900

    [feat] 몬스터 데이터 관련 추가
    - 능력치 추가
    - 설정 능력치 추가(SO or CSV로 대체 예정)

---
+++ Assets/YSJ/Scripts/Monster/MonsterConfig.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PVS.Monster
+{
+    // 외부 값 변경용
+    public class MonsterConfig : MonoBehaviour
+    {
+        [field: SerializeField] public float Hp { get; private set; }
+        [field: SerializeField] public float ATK { get; private set; }
+        [field: SerializeField] public float SPD { get; private set; }
+
+        [field: SerializeField] public float AtkRange { get; private set; }
+        [field: SerializeField] public int ExpPoint { get; private set; }
+
+        // 드롭 아이템 넣을 수 있게 만들기
+        // 확률 필수
+        // 예) 골드 / 0.3f
+    }
+}
