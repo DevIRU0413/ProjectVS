a3383db643448d0b8c4d9052c6b24c2e816cb482
DevIRU <deviru0413@gmail.com>
2025-06-24 02:52:48 +0900

    [feat] 몬스터 기본 상태 클래스 추가
    - MonsterState.cs 추가

---
+++ Assets/YSJ/Scripts/Monster/State/MonsterState.cs
@@ -0,0 +1,13 @@
+﻿namespace PVS.Monster.State
+{
+    public abstract class MonsterState
+    {
+        protected MonsterController _controller;
+
+        public MonsterState(MonsterController controller) => _controller = controller;
+
+        public virtual void Enter() { }
+        public virtual void Update() { }
+        public virtual void Exit() { }
+    }
+}
