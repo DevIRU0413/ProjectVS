9bf9381212f08a73e46902dfb8d1ea3ae75e4be7
DevIRU <deviru0413@gmail.com>
2025-06-24 02:40:16 +0900

    [feat] 공용 기능들 추가
    - 컬라이더 충돌 확인 중간 처리 CS 추가
    - UnityUtilEx 추가
    - Enums 추가

---
+++ Assets/YSJ/Scripts/Enums.cs
@@ -0,0 +1,22 @@
+﻿public enum PlayerStateType
+{
+    Idle,
+    Move,
+    Death,
+}
+
+public enum MonsterStateType
+{
+    None,
+    Idle,
+    Move,
+    Win,
+    Death,
+}
+
+public enum MonsterPattenState
+{
+    None,
+    Play,
+    Done,
+}
---
+++ Assets/YSJ/Scripts/UnityUtilEx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Util
+{
+    public static class UnityUtilEx
+    {
+        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
+        {
+            var comp = go.GetComponent<T>();
+            if (comp == null)
+                comp = go.AddComponent<T>();
+            return comp;
+        }
+
+        public static GameObject FindOrCreateGameObject(string name) 
+        {
+            GameObject go = GameObject.Find(name);
+            if (go == null)
+                go = new GameObject(name);
+            return go;
+        }
+
+        public static T FindNearestTarget<T>(this Transform origin, List<GameObject> targets, Func<GameObject, bool> condition = null) where T : Component
+        {
+            float minDist = float.MaxValue;
+            GameObject nearest = null;
+            Vector3 originPos = origin.position;
+
+            foreach (var obj in targets)
+            {
+                if (obj == null) continue;
+
+                var target = obj;
+                if(condition(target) == false) continue;
+
+                float dist = Vector2.Distance(originPos, target.transform.position);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    nearest = target;
+                }
+            }
+
+            return nearest?.GetComponent<T>();
+        }
+    }
+}
