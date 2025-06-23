3aaf6285cceae41cc72cbc166b6bb61f01e35205
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-24 00:00:49 +0900

    feat: 설정창 버튼 메서드 구현

---
+++ Assets/Min/Scripts/UIs/PanelBehaviours/SettingsPanelButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProjectVS.Utils.UIManager;
+
+
+namespace ProjectVS.UIs.PanelBehaviours.SettingsPanelButtons
+{
+    public class SettingsPanelButtons : MonoBehaviour
+    {
+        public void OnClickESCButton()
+        {
+            UIManager.Instance.CloseTopPanel();
+        }
+    }
+}
