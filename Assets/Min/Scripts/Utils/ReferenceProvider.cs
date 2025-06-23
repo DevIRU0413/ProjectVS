b374e23b0f6b3f50f686356ed200075accbe86ba
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-23 16:03:59 +0900

    feat: 유틸 클래스 추가
    - 제네릭 싱글톤 클래스 Singleton, SceneSingleton 추가
    - 컴포넌트 캐싱 클래스 ReferenceRegistry, ReferenceProvider 추가
    - 자료구조 클래스 PriorityQueue 추가
    - 옵저버 패턴용 클래스 ObservableProperty 추가
    - 오브젝트 풀용 클래스 ObjectPool, PooledObject 추가

---
+++ Assets/Min/Scripts/Utils/ReferenceProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using ProjectVS.Utils.ReferenceRegistry;
+
+
+namespace ProjectVS.Utils.ReferenceProvider
+{
+    //[DisallowMultipleComponent]
+    //public class ReferenceProvider : MonoBehaviour
+    //{
+    //    [SerializeField] private Component _component;
+
+    //    private void Awake()
+    //    {
+    //        if (_component == null)
+    //        {
+    //            _component = GetComponent<IDamageable>() as Component;
+    //            _component = GetComponent<Component>() as Component;
+    //        }
+
+    //        ReferenceRegistry.Register(this);
+    //    }
+    //    private void OnDestroy() => ReferenceRegistry.Unregister(this);
+
+    //    public T GetAs<T>() where T : class
+    //    {
+    //        return _component as T;
+    //    }
+    //}
+}
---
+++ Assets/Min/Scripts/Utils/ReferenceRegistry.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProjectVS.Utils.ReferenceProvider;
+
+
+namespace ProjectVS.Utils.ReferenceRegistry
+{
+    //public static class ReferenceRegistry
+    //{
+    //    private static Dictionary<GameObject, ReferenceProvider> _providers = new();
+
+    //    public static void Register(ReferenceProvider referenceProvider)
+    //    {
+    //        if (_providers.ContainsKey(referenceProvider.gameObject)) return;
+
+    //        _providers.Add(referenceProvider.gameObject, referenceProvider);
+    //    }
+
+    //    public static void Unregister(ReferenceProvider referenceProvider)
+    //    {
+    //        if (!_providers.ContainsKey(referenceProvider.gameObject)) return;
+
+    //        _providers.Remove(referenceProvider.gameObject);
+    //    }
+
+    //    public static void Clear()
+    //    {
+    //        _providers.Clear();
+    //    }
+
+    //    public static ReferenceProvider GetProvider(GameObject gameObject)
+    //    {
+    //        if (!_providers.ContainsKey(gameObject)) return null;
+
+    //        return _providers[gameObject];
+    //    }
+    //}
+}
