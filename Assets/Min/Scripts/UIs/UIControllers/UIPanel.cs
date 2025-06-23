cdff41406458edf04dcacd5db374202c274812f4
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-23 23:59:31 +0900

    feat: UIManager의 Stack용 클래스 구현
    - 해당 컴포넌트를 가진 UI만을 Stack에 넣기 위해 의미적인 클래스 구현

---
+++ Assets/Min/Scripts/UIs/UIControllers/UIPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+namespace ProjectVS.UIs.UIControllers.UIPanel
+{
+    public class UIPanel : MonoBehaviour
+    {
+        public string PanelKey { get; private set; }
+    }
+}
