27b375c6705ced8b805be0c6a8b3c271fe50218d
DevIRU <deviru0413@gmail.com>
2025-06-24 02:44:10 +0900

    [test] Test용 플레이어 데이터 관련 추가
    - 플레이어 능력치 추가
    - 플레이어 설정 능력치 추가(SO or CSV로 대체 예정)
    - 플레이어 FSM에 사용할 기본 상태 추가

---
+++ Assets/YSJ/Scripts/Player/PlayerConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PVS.Player
+{
+    // 외부 값 변경용
+    public class PlayerConfig : MonoBehaviour
+    {
+        [field: SerializeField] public float Hp { get; private set; }
+        [field: SerializeField] public float ATK { get; private set; }
+        [field: SerializeField] public float SPD { get; private set; }
+
+        [field: SerializeField] public float AtkRAnge { get; private set; }
+    }
+}
---
+++ Assets/YSJ/Scripts/Player/State/PlayerState.cs
@@ -0,0 +1,13 @@
+﻿namespace PVS.Player.State
+{
+    public abstract class PlayerState
+    {
+        protected PlayerController _controller;
+
+        public PlayerState(PlayerController controller) => _controller = controller;
+
+        public virtual void Enter() { }
+        public virtual void Update() { }
+        public virtual void Exit() { }
+    }
+}
