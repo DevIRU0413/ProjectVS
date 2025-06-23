e3b92b3d6b75e5c1eff940abfcb7c1a4cc52e923
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-23 23:56:23 +0900

    feat: 별개의 UI 그룹을 하나로 관리하는 시스템 구현
    - 각각의 UIButtonAnimator를 묶어서 한 번에 제어하도록 하기 위해 구현
    - Show, Hide를 한 번에 호출하여 관리

---
+++ Assets/Min/Scripts/UIs/UIControllers/UIButtonGroupController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+using UIButtonAnimatorClass = ProjectVS.UIs.UIBase.UIButtonAnimator.UIButtonAnimator;
+
+
+namespace ProjectVS.UIs.UIBase.UIButtonGroupController
+{
+    public class UIButtonGroupController : MonoBehaviour
+    {
+        [SerializeField] private List<UIButtonAnimatorClass> _group;
+
+        public void AnimateIn(Action onComplete = null)
+        {
+            StartCoroutine(AnimateGroupsIn(onComplete));
+        }
+
+        public void AnimateOut(Action onComplete = null)
+        {
+            StartCoroutine(AnimateGroupsOut(onComplete));
+        }
+
+        public void ShowInstant()
+        {
+            foreach (var btn in _group)
+                btn.ShowInstant();
+        }
+
+        public void HideInstant()
+        {
+            foreach (var btn in _group)
+                btn.HideInstant();
+        }
+
+        private IEnumerator AnimateGroupsIn(Action onComplete = null)
+        {
+            foreach (var btn in _group)
+            {
+                yield return StartCoroutine(btn.AnimateIn());
+            }
+
+            onComplete?.Invoke();
+        }
+
+        private IEnumerator AnimateGroupsOut(Action onComplete = null)
+        {
+            for (int i = _group.Count - 1; i >= 0; i--) // 역순으로 닫기
+            {
+                yield return StartCoroutine(_group[i].AnimateOut());
+            }
+
+            onComplete?.Invoke();
+        }
+    }
+}
