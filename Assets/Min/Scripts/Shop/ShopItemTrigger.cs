699e9161a2f18ae04ac91db8ffafc468e9f07eaf
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-24 21:55:05 +0900

    feat: 아이템의 Trigger 기능 구현
    - ScriptableObject의 Asset을 아이템으로 사용하여 플레이어와 Trigger로 상호작용하도록 만든 클래스
    - 아이템 구매 시 X 표시 및 재구매 불가 기능 추가
    - 아이템 구매 시 상인의 감사 메시지 호출

---
+++ Assets/Min/Scripts/Shop/ShopItemTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ThanksTextIndicatorClass = ProjectVS.Shop.ThanksTextIndicator.ThanksTextIndicator;
+
+namespace ProjectVS.Shop.ShopItemTrigger
+{
+    public class ShopItemTrigger : MonoBehaviour
+    {
+        [SerializeField] private ThanksTextIndicatorClass _thanks;
+
+        public ScriptableObject ItemSO;
+
+
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private LayerMask _playerLayer;
+        [SerializeField] private Sprite _xSprite; // X 표시용 스프라이트
+
+        private bool _isPurchased = false;
+
+        private void Awake()
+        {
+            if (_spriteRenderer == null)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (((1 << collision.gameObject.layer) & _playerLayer) != 0)
+            {
+                if (_isPurchased) return;
+
+                // ItemSO 의 가격 판단
+                // if (ItemSO.price > Player.Money) return
+                // else Player.Money - ItemSO.price;
+
+                // 상점 NPC 말풍선 팝업
+
+                // 아이템 업그레이드
+                // ItemSO?.OnClick();
+
+                // X 표시
+                _spriteRenderer.sprite = _xSprite;
+
+                // 감사 메시지 표시
+                _thanks.ShowThanksText();
+
+                _isPurchased = true;
+            }
+        }
+    }
+
+}
