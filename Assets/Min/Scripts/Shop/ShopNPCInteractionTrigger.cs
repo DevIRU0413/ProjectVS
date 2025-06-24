dc65eb6b3d0a50a5534a3019d6e06034007a31cc
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-24 21:56:58 +0900

    feat: 상인 NPC의 대화 가능 영역에 대한 시각적 피드백 기능 추가
    - 상인의 특정 범위 안에 플레이어 TriggerEnter 시 특정 키를 통해 상호작용 가능에 대한 시각적 피드백 기능 추가
    - 플레이어가 사용할 CanInteract 프로퍼티 추가

---
+++ Assets/Min/Scripts/Shop/ShopNPCInteractionTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProjectVS.Shop.ShopNPCInteractionTrigger
+{
+    public class ShopNPCInteractionTrigger : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _playerLayer;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private GameObject _eventSelectPanel;
+
+
+        private bool _canInteract = false;
+        public bool CanInteract => _canInteract;    // 해당 프로퍼티가 true일 때만 상호작용 가능하도록 프로퍼티 사용
+
+
+        private void Awake()
+        {
+            if (_spriteRenderer == null)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (((1 << collision.gameObject.layer) & _playerLayer) != 0)
+            {
+                _canInteract = true;
+
+                _spriteRenderer.enabled = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (((1 << collision.gameObject.layer) & _playerLayer) != 0)
+            {
+                _canInteract = false;
+
+                _spriteRenderer.enabled = false;
+            }
+        }
+    }
+}
