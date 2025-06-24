e53d44582349fda9b8146c07fe756df5593d1789
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-24 21:59:10 +0900

    feat: SpriteRenderer의 Fade 기능 추가
    - 게임 시작 시 사용할 Sprite의 Fade In and Out 효과 구현

---
+++ Assets/Min/Scripts/Sprite Effector/StartSpriteEffector.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace ProjectVS.CutSceneEffectors.StartSpriteEffector
+{
+    public class StartSpriteEffector : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+
+        [Header("Fade In/Out Settings")]
+        [SerializeField, Range(0f, 1f)] private float _frameRate = 0.02f;
+        [SerializeField, Range(0, 255)] private int _fadeStep = 5;
+
+
+        private void OnEnable()
+        {
+            StartCoroutine(IE_FadeInAndOut());
+        }
+
+        private IEnumerator IE_FadeInAndOut()
+        {
+            WaitForSeconds wait = new WaitForSeconds(_frameRate);
+
+            Color color = _spriteRenderer.color;
+            int alpha = 0;
+            while (alpha < 255)
+            {
+                alpha += _fadeStep;
+                color.a = Mathf.Clamp01(alpha / 255f);
+                _spriteRenderer.color = color;
+                yield return wait;
+            }
+
+            while (alpha > 0)
+            {
+                alpha -= _fadeStep;
+                color.a = Mathf.Clamp01(alpha / 255f);
+                _spriteRenderer.color = color;
+                yield return wait;
+            }
+
+            //SceneManager.LoadScene(1);
+            Debug.Log("다음 씬 이동");
+        }
+    }
+}
