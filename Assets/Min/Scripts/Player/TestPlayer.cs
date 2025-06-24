7eda2cb9c3f265f2d67f50be841a9e8648bc3da1
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-24 21:48:57 +0900

    test: 테스트용 플레이어 클래스 추가
    - 이동 및 NPC 상호작용 기능 구현

---
+++ Assets/Min/Scripts/Player/TestPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using ProjectVS.Utils.UIManager;
+
+using UnityEngine;
+
+using ShopNPCInteractionTriggerClass = ProjectVS.Shop.ShopNPCInteractionTrigger.ShopNPCInteractionTrigger;
+
+
+namespace ProjectVS.Player.TestMove
+{
+    public class TestPlayer : MonoBehaviour
+    {
+        [SerializeField] ShopNPCInteractionTriggerClass _shopNPCInteractionTrigger;
+
+        [SerializeField] private float _moveSpeed = 5f;
+        private Vector2 _moveDirection;
+        public Vector2 MoveDirection => _moveDirection;
+
+
+        private void Update()
+        {
+            GetInput();
+            InteractWithNPC();
+        }
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+
+        private void GetInput()
+        {
+            _moveDirection = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                _moveDirection += Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                _moveDirection += Vector2.down;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                _moveDirection += Vector2.left;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                _moveDirection += Vector2.right;
+            }
+
+            _moveDirection = _moveDirection.normalized;
+        }
+
+        private void Move()
+        {
+            transform.Translate(_moveDirection * _moveSpeed * Time.deltaTime);
+        }
+
+        private void InteractWithNPC()
+        {
+            if (Input.GetKeyDown(KeyCode.E) && _shopNPCInteractionTrigger.CanInteract)
+            {
+                UIManager.Instance.Show("Event Select Panel");
+            }
+        }
+    }
+}
