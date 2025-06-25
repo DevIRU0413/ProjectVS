b81f93f2a69cff193c9c2f8b2bd7b28a415d5cc8
JungDaWit <ekdnlt10@gmail.com>
2025-06-25 14:11:23 +0900

    [docs] 플레이어 충돌 관련 주석 수정

--- Assets/JDW/Assets/Scenes/Player/Script/PlayerMove.cs
+++ Assets/JDW/Assets/Scenes/Player/Script/PlayerMove.cs
@@ -11,7 +11,6 @@ public class PlayerMove : MonoBehaviour
     public MapSwitcer mapSwitcer;
     public Vector2 MoveInput;
     public Animator anim;
-    private Vector2? _blockedDirection = null;
 
     public FadeManager fadeManager;
     public Timer timer;
@@ -41,15 +40,7 @@ public class PlayerMove : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
         Vector2 input = new Vector2(h, v).normalized;
 
-        // 차단된 방향일 경우 입력 무시
-      // if (_blockedDirection.HasValue)
-      // {
-      //     float dot = Vector2.Dot(input, _blockedDirection.Value);
-      //     if (dot > 0.7f) // 같은 방향이면 막기
-      //     {
-      //         input = Vector2.zero;
-      //     }
-      // }
+       
 
         MoveInput = input;
 
@@ -60,26 +51,13 @@ public class PlayerMove : MonoBehaviour
         bool IsWalking = MoveInput != Vector2.zero;
         anim.SetBool("IsWalking", IsWalking);
     }
-    public void BlockDirection(Vector2 dir)
-    {
-        _blockedDirection = dir.normalized;
-    }
-    public void ClearBlockedDirection()
-    {
-        _blockedDirection = null;
-    }
-    private void OnTriggerEnter2D(Collider2D other) // 충돌 시작
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Door")) // 문과 충돌 했을시
         {
             Debug.Log("상점을 나감");
             StartCoroutine(HandleFadeTransition());
         }
-    }
-    private void OnTriggerExit2D(Collider2D other) // 충돌 종료
-    {
-        if (other.CompareTag("Obstacle")) 
-            ClearBlockedDirection();
     }
     private IEnumerator HandleFadeTransition()
     {
