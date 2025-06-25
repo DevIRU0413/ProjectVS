1b39dfd0461d1fc8f96fc510c854c93da634da1c
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-25 14:11:48 +0900

    feat: 로그 텍스트 시스템 구현 중
    - Content Size Fitter, Vertical Layout Group 컴포넌트를 활용해 로그 시스템 구현 중
    - 현재 테스트용 메서드만 구현 상태

---
+++ Assets/Min/Scripts/Dialogue/DialogueLogManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+
+
+namespace ProjectVS.Dialogue.DialogueLogManager
+{
+    public class DialogueLogManager : MonoBehaviour
+    {
+        [SerializeField] private Transform _logContentTransform;
+        [SerializeField] private GameObject _logBoxPrefab;
+
+        private List<GameObject> _logBoxes = new();
+
+        // TODO: 오브젝트 풀링 등 해야될 듯
+        // TODO: GetComponent도 안할 방법 찾아야 될 듯
+        public void AddLogBox(string content)
+        {
+            GameObject box = Instantiate(_logBoxPrefab, _logContentTransform);
+            TMP_Text text = box.GetComponentInChildren<TMP_Text>();
+            text.text = content;
+
+            _logBoxes.Add(box);
+        }
+
+        // MEMO: 순회 중 Destroy 됐던가??
+        public void ClearLog()
+        {
+            foreach (GameObject box in _logBoxes)
+            {
+                Destroy(box);
+            }
+
+            _logBoxes.Clear();
+        }
+    }
+}
