9763626eef6ee532adf862e89308629ce71a3b57
JungDaWit <ekdnlt10@gmail.com>
2025-06-25 11:59:04 +0900

    [refactor] 장애물 스크립트 제거

--- Assets/JDW/Assets/Scenes/Player/Script/Player.cs
+++ Assets/JDW/Assets/Scenes/Player/Script/Player.cs
@@ -15,47 +15,13 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layer = collision.gameObject.layer;
-
-        if(layer == LayerMask.NameToLayer("HpDown"))
-        {
-            Debug.Log("플레이어 체력 감소");
-            Hp--;
-        }
-        if (layer == LayerMask.NameToLayer("HpUp"))
-        {
-            Debug.Log("플레이어 체력 증가");
-            Hp ++;
-        }
-        if (layer == LayerMask.NameToLayer("SpeedUp"))
-        {
-            Debug.Log("플레이어 속도 증가");
-            MoveSpeed ++;
-        }
-        if (layer == LayerMask.NameToLayer("PowerUp"))
-        {
-            Debug.Log("플레이어 공격력 증가");
-            Power ++;
-        }
-        if (layer == LayerMask.NameToLayer("MagicAttackSpeed"))
-        {
-            Debug.Log("플레이어 마법 공격 속도 증가");
-            MagicAttackSpeed -= 0.1f;
-        }
-        if (layer == LayerMask.NameToLayer("DefenseUp"))
-        {
-            Debug.Log("플레이어 방어력 증가");
-            Defense++;
-        }
-        if (layer == LayerMask.NameToLayer("AxAttackSpeed"))
-        {
-            Debug.Log("플레이어 도끼 공격 속도 증가");
-            AxAttackSpeed -= 0.1f;
-        }
-        if (layer == LayerMask.NameToLayer("SwordAttackSpeed"))
-        {
-            Debug.Log("플레이어 검 공격 속도 증가");
-            SwordAttackSpeed -= 0.1f;
-        }
-        
+        if (layer == LayerMask.NameToLayer("HpDown")) Hp--;
+        if (layer == LayerMask.NameToLayer("HpUp")) Hp++;
+        if (layer == LayerMask.NameToLayer("SpeedUp")) MoveSpeed++;
+        if (layer == LayerMask.NameToLayer("PowerUp")) Power++;
+        if (layer == LayerMask.NameToLayer("DefenseUp")) Defense++;
+        if (layer == LayerMask.NameToLayer("MagicAttackSpeed")) MagicAttackSpeed -= 0.1f;
+        if (layer == LayerMask.NameToLayer("AxAttackSpeed")) AxAttackSpeed -= 0.1f;
+        if (layer == LayerMask.NameToLayer("SwordAttackSpeed")) SwordAttackSpeed -= 0.1f;
     }
 }
