d4444f312eddbd69475079cdd58b323ef2bf126b
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-25 11:05:13 +0900

    feat: 한 글자씩 텍스트 출력 및 스킵 기능 추가

---
+++ Assets/Min/Scripts/Dialogue/TextEffect/DialogueTextTyper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TextTyperBaseClass = ProjectVS.Dialogue.TextEffect.TextTyperBase.TextTyperBase;
+
+
+namespace ProjectVS.Dialogue.TextEffect.DialogueTextTyper
+{
+    public class DialogueTextTyper : TextTyperBaseClass
+    {
+        private void Awake()
+        {
+            if (_text == null)
+            {
+                _text = GetComponent<TMPro.TMP_Text>();
+            }
+        }
+    }
+}
---
+++ Assets/Min/Scripts/Dialogue/TextEffect/TextTyperBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using System.Text;
+
+using UnityEngine;
+
+
+namespace ProjectVS.Dialogue.TextEffect.TextTyperBase
+{
+    public class TextTyperBase : MonoBehaviour
+    {
+        [SerializeField] protected TMP_Text _text;
+        [SerializeField] protected float _typingSpeed = 0.05f;
+
+        protected Coroutine _typingCo;
+        protected string _currentContent;
+
+        public virtual void StartTyping(string content)
+        {
+            if (_typingCo != null)
+            {
+                StopCoroutine(_typingCo);
+            }
+
+            _currentContent = content;
+            _typingCo = StartCoroutine(IE_Type(content));
+        }
+
+
+        public virtual void Skip()
+        {
+            if (_typingCo != null)
+            {
+                StopCoroutine(_typingCo);
+                _typingCo = null;
+                _text.text = _currentContent;
+            }
+        }
+
+        protected virtual IEnumerator IE_Type(string content)
+        {
+            _text.text = "";
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in content)
+            {
+                sb.Append(c);
+                _text.text = sb.ToString();
+                yield return new WaitForSeconds(_typingSpeed);
+            }
+
+            _typingCo = null;
+        }
+    }
+}
