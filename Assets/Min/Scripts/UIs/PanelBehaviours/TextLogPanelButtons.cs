1de46722e881e41bb9cb7783ff7121c6e2a2c8ee
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-26 19:40:40 +0900

    feat: 텍스트 로그 창 버튼 시스템 구현
    - 텍스트 로그 창 호출 방법 변경에 따른 추후 사용 중지할 수 있는 클래스

---
+++ Assets/Min/Scripts/UIs/PanelBehaviours/TextLogPanelButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+namespace ProjectVS.UIs.PanelBehaviours.TextLogPanelButtons
+{
+    public class TextLogPanelButtons : MonoBehaviour
+    {
+        public void OnClickESCButton()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
