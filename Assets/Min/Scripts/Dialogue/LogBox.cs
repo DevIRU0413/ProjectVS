dfa6f6343ed7f199b60bbb181b29f6f15727e0b1
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-26 19:32:00 +0900

    feat: 대화 시스템의 이름과 텍스트 내용 분리 시스템 추가
    - 이름은 대화 내용과 달리 텍스트 애니메이션이 적용되지 않아야 하기 때문에 구조 분리

---
+++ Assets/Min/Scripts/Dialogue/LogBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+
+
+namespace ProjectVS.Dialogue.LogBox
+{
+    public class LogBox : MonoBehaviour
+    {
+        public TMP_Text NameText;
+        public TMP_Text ContentText;
+    }
+}
