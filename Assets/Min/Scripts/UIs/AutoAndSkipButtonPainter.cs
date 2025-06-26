4d7592f6e4299ec2c62006ac8b3d417c16348629
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-26 22:27:15 +0900

    refactor: EventPanelButtons 클래스 책임 분리
    - Skip, Auto버튼이 Event Panel에 종속되지 않고 독립적으로 실행되는 경우가 존재하여 책임을 분리

---
+++ Assets/Min/Scripts/UIs/AutoAndSkipButtonPainter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using ProjectVS.Dialogue.TextEffect.DialogueTextTyper;
+using ProjectVS.Utils.UIManager;
+using DialogueManagerClass = ProjectVS.Dialogue.DialogueManager.DialogueManager;
+
+public class AutoAndSkipButtonPainter : MonoBehaviour
+{
+    [SerializeField] private Image _skipButtonImage;
+    [SerializeField] private Image _autoButtonImage;
+
+    [SerializeField] private DialogueTextTyper _eventDialogueTextTyper;
+    [SerializeField] private DialogueManagerClass _dialogueManager;
+
+    private Color _toggledColor = new Color(0.4f, 0.9f, 0.95f, 1f); // 하늘색 임의 지정
+
+    private void OnEnable()
+    {        
+        _eventDialogueTextTyper.IsSkipToggled.Subscribe(CheckSkipToggled);
+        _dialogueManager.IsAutoMode.Subscribe(CheckAutoToggled);
+    }
+
+    private void Start()
+    {
+        CheckSkipToggled();
+        CheckAutoToggled();
+    }
+
+    private void OnDisable()
+    {
+        if (_eventDialogueTextTyper != null)
+        {
+            _eventDialogueTextTyper.IsSkipToggled.Unsubscribe(CheckSkipToggled);
+        }
+        if (_dialogueManager != null)
+        {
+            _dialogueManager.IsAutoMode.Unsubscribe(CheckAutoToggled);
+        }
+    }
+
+
+    private void CheckSkipToggled()
+    {
+        if (_eventDialogueTextTyper.IsSkipToggled.Value)
+        {
+            _skipButtonImage.color = _toggledColor;
+        }
+        else
+        {
+            _skipButtonImage.color = Color.white;
+        }
+    }
+
+    private void CheckAutoToggled()
+    {
+        if (_dialogueManager.IsAutoMode.Value)
+        {
+            _autoButtonImage.color = _toggledColor;
+        }
+        else
+        {
+            _autoButtonImage.color = Color.white;
+        }
+    }
+}
--- Assets/Min/Scripts/UIs/PanelBehaviours/EventPanelButtons.cs
+++ Assets/Min/Scripts/UIs/PanelBehaviours/EventPanelButtons.cs
@@ -11,32 +11,6 @@ namespace ProjectVS.UIs.PanelBehaviours.EventPanelButtons
 {
     public class EventPanelButtons : MonoBehaviour
     {
-        [SerializeField] private Image _skipButtonImage;
-        [SerializeField] private Image _autoButtonImage;
-
-        [SerializeField] private DialogueTextTyper _eventDialogueTextTyper;
-        [SerializeField] private DialogueManagerClass _dialogueManager;
-
-        private Color _toggledColor = new Color(0.4f, 0.9f, 0.95f, 1f); // 하늘색 임의 지정
-
-        private void Start()
-        {
-            _eventDialogueTextTyper.IsSkipToggled.Subscribe(CheckSkipToggled);
-            _dialogueManager.IsAutoMode.Subscribe(CheckAutoToggled);
-        }
-
-        private void OnDisable()
-        {
-            if (_eventDialogueTextTyper != null)
-            {
-                _eventDialogueTextTyper.IsSkipToggled.Unsubscribe(CheckSkipToggled);
-            }
-            if (_dialogueManager != null)
-            {
-                _dialogueManager.IsAutoMode.Unsubscribe(CheckAutoToggled);
-            }
-        }
-
         public void OnClickESCButton()
         {
             // TODO: 만약 이 씬이 최하단 씬이 아니라는 보장이 없다면 ForceCloseTopPanel 사용
@@ -47,30 +21,6 @@ namespace ProjectVS.UIs.PanelBehaviours.EventPanelButtons
         {
             UIManager.Instance.Hide("Event Panel");
             UIManager.Instance.Show("Log Panel");
-        }
-
-        private void CheckSkipToggled()
-        {
-            if (_eventDialogueTextTyper.IsSkipToggled.Value)
-            {
-                _skipButtonImage.color = _toggledColor;
-            }
-            else
-            {
-                _skipButtonImage.color = Color.white;
-            }
-        }
-
-        private void CheckAutoToggled()
-        {
-            if (_dialogueManager.IsAutoMode.Value)
-            {
-                _autoButtonImage.color = _toggledColor;
-            }
-            else
-            {
-                _autoButtonImage.color = Color.white;
-            }
         }
     }
 }
