cca31d438abda005386e13365b176ac974c3fc7b
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-26 21:05:29 +0900

    feat: 선택지 버튼 클릭시 클릭한 텍스트가 로그에 반영되기 위한 로직 구현
    - Action을 매개변수로 받아 클릭된 버튼에 따라 다른 텍스트가 로그에 반영되도록 구현

---
+++ Assets/Min/Scripts/Dialogue/ChoiceDialogueManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+
+namespace ProjectVS.Dialogue.ChoiceDialogueManager
+{
+    public class ChoiceDialogueManager : MonoBehaviour
+    {
+        [Header("선택창 오브젝트 등록")]
+        [SerializeField] GameObject _choiceButtonsPanel;
+        [SerializeField] Button _upperChoiceButton;
+        [SerializeField] Button _lowerChoiceButton;
+        [SerializeField] TMP_Text _upperChoiceText;
+        [SerializeField] TMP_Text _lowerChoiceText;
+
+        private Action _onUpperClick;
+        private Action _onLowerClick;
+
+        public void ShowChoiceButtons(string upperContent, string lowerContent, Action onUpperClick, Action onLowerClick)
+        {
+            Debug.Log($"[ChoiceDialogueManager] ShowChoiceButtons 메서드 호출 됨");
+
+            _choiceButtonsPanel.SetActive(true);
+
+            _upperChoiceText.text = upperContent;
+            _lowerChoiceText.text = lowerContent;
+
+            _onUpperClick = onUpperClick;
+            _onLowerClick = onLowerClick;
+
+            _upperChoiceButton.onClick.RemoveAllListeners();
+            _lowerChoiceButton.onClick.RemoveAllListeners();
+
+            _upperChoiceButton.onClick.AddListener(() =>
+            {
+                _onUpperClick?.Invoke();
+                _choiceButtonsPanel.SetActive(false);
+            });
+            _lowerChoiceButton.onClick.AddListener(() =>
+            {
+                _onLowerClick?.Invoke();
+                _choiceButtonsPanel.SetActive(false);
+            });
+        }
+    }
+}
