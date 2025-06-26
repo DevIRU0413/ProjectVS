893f54bf1a9c400492e95dd2f80f1659d6bf269b
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-26 19:38:59 +0900

    feat: 이벤트 진입 가능 여부에 따른 버튼 색 변경 시스템 구현

--- Assets/Min/Scripts/UIs/PanelBehaviours/EventSelectPanelButtons.cs
+++ Assets/Min/Scripts/UIs/PanelBehaviours/EventSelectPanelButtons.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 using ProjectVS.Utils.UIManager;
 using DialogueManagerClass = ProjectVS.Dialogue.DialogueManager.DialogueManager;
+using UnityEngine.UI;
 
 
 namespace ProjectVS.UIs.PanelBehaviours.EventSelectPanelButtons
@@ -11,10 +12,22 @@ namespace ProjectVS.UIs.PanelBehaviours.EventSelectPanelButtons
     public class EventSelectPanelButtons : MonoBehaviour
     {
         [SerializeField] private DialogueManagerClass _dialogueManager;
+        [SerializeField] private Button _eventButton;
+
+        private ColorBlock _eventButtonColors;
+        private Color _clickableNormalColor;
+        private Color _unClickableNormalColor;
+
+        private void Awake()
+        {
+            InitColor();
+        }
 
         private void OnEnable()
         {
             _dialogueManager.ShowRepeatDialogue();
+
+            CheckDisableButton();
 
             Debug.Log("[EventSelectPanelButtons] 이벤트 선택 패널 버튼 활성화됨.");
         }
@@ -36,6 +49,28 @@ namespace ProjectVS.UIs.PanelBehaviours.EventSelectPanelButtons
 
             UIManager.Instance.Hide("Event Select Panel");
             UIManager.Instance.Show("Event Panel");
+        }
+
+        private void InitColor()
+        {
+            _clickableNormalColor = _eventButton.colors.selectedColor;
+            _unClickableNormalColor = new Color(0.3f, 0.3f, 0.3f, 1f); // 회색 임의 지정
+
+            _eventButtonColors = _eventButton.colors;
+            _eventButtonColors.disabledColor = _unClickableNormalColor;
+            _eventButton.colors = _eventButtonColors;
+        }
+
+        private void CheckDisableButton()
+        {
+            if (!_dialogueManager.CanShowEventDialogue())
+            {
+                _eventButton.interactable = false;
+            }
+            else
+            {
+                _eventButton.interactable = true;
+            }
         }
     }
 }
