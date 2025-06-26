2d5f223d1b5f7d09023821123a0d1bf643fa818a
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-26 21:02:55 +0900

    feat: 스탠딩 이미지와 풀샷 이미지를 묶기 위한 클래스 생성

---
+++ Assets/Min/Scripts/Character Images/StandingToFullBodyShotPair.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace ProjectVS.CharacterImages.StandingToFullBodyShotPair
+{
+    [System.Serializable]
+    public class StandingToFullBodyShotPair
+    {
+        public Button StandingShotButton;
+        public GameObject FullBodyShotPrefab; // 해당 프리팹은 [UI Panel, UI Button On Panel, UI Button Group Controller, UI Animator] 등록하고 UI Manager 관리 하에 사용할 듯
+    }
+}
