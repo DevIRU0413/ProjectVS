0c327b245e03b2ae4760646e3db2f4444f241033
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-26 19:37:29 +0900

    feat: 호감도 바 사용 방법 변경
    - 임의로 Slider의 value로 사용했던 호감도 바의 Image 컴포넌트 사용으로 인한 Image.fillAmount로 사용 방법 변경

---
+++ Assets/Min/Scripts/UIs/AffinityUI/AffinityBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using NPCAffinityModelClass = ProjectVS.Shop.NPCAffinityModel.NPCAffinityModel;
+
+
+namespace ProjectVS.UIs.AffinityBar
+{
+    public class AffinityBar : MonoBehaviour
+    {
+        [Header("호감도 바 참조")]
+        [SerializeField] Image _affinityFillImage;
+        [SerializeField] TMP_Text _affinityValueText;
+
+        [Header("호감도 모델 참조")]
+        [SerializeField] NPCAffinityModelClass _affinityModel;
+
+        private void OnEnable()
+        {
+            RenewAffinity();
+        }
+
+
+        private void RenewAffinity()
+        {
+            _affinityValueText.text = _affinityModel.Affinity.ToString();
+
+            float fillAmount = _affinityModel.Affinity / 100f;
+            _affinityFillImage.fillAmount = fillAmount;
+        }
+    }
+}
