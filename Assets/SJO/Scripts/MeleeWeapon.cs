c7aa8ca52665ce54c20310469fb6a87cbdfb006c
DevNap <thdwodh789@gmail.com>
2025-06-27 17:59:09 +0900

    [Fix] 작동 안되는 스크립트 및 연동 부분 수정
    아직 전체 수정이 되지 않음.
    원거리 스킬 쪽이 근접 스킬과 동일한 방식으로 나오고 있음.

--- Assets/SJO/Scripts/MeleeWeapon.cs
+++ Assets/SJO/Scripts/MeleeWeapon.cs
@@ -1,14 +1,14 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeWeapon : MonoBehaviour
 {
-    // µ¥¹ÌÁö, °üÅë·Â
+    // ë°ë¯¸ì§€, ê´€í†µë ¥
     public float damage;
     public int penetratingPower;
 
-    // ÃÑ¾Ë ÂÊ. ³ªÁß¿¡´Â ³ª´²¾ßµÊ
+    // ì´ì•Œ ìª½. ë‚˜ì¤‘ì—ëŠ” ë‚˜ëˆ ì•¼ë¨
     private Rigidbody2D _rigid;
 
     private void Awake()
@@ -16,13 +16,13 @@ public class MeleeWeapon : MonoBehaviour
         _rigid = GetComponent<Rigidbody2D>();
     }
 
-    // ÃÊ±âÈ­
+    // ì´ˆê¸°í™”
     public void Init(float damage, int penetratingPower, Vector3 dir)
     {
         this.damage = damage;
         this.penetratingPower = penetratingPower;
 
-        // °üÅë·ÂÀÌ ¹«ÇÑº¸´Ù(-1) Å©´Ù¸é ¼Óµµ°¡ ÀÖÀ½
+        // ê´€í†µë ¥ì´ ë¬´í•œë³´ë‹¤(-1) í¬ë‹¤ë©´ ì†ë„ê°€ ìˆìŒ
         if (penetratingPower > -1)
         {
             _rigid.velocity = dir * 15f;
@@ -30,12 +30,12 @@ public class MeleeWeapon : MonoBehaviour
     }
 
     /// <summary>
-    /// Enemy tag·Î ¹İÀÀÇÏ´Â ÇÔ¼ö. ÇÊ¿ä½Ã »èÁ¦ ¶Ç´Â tag º¯°æ
+    /// Enemy tagë¡œ ë°˜ì‘í•˜ëŠ” í•¨ìˆ˜. í•„ìš”ì‹œ ì‚­ì œ ë˜ëŠ” tag ë³€ê²½
     /// </summary>
     /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Enemy") || penetratingPower == -1) return;
+        if (!collision.CompareTag("Monster") || penetratingPower == -1) return;
 
         penetratingPower--;
 
