dc09d25eb04be88ce75f335f474f1f61bd7f3ef7
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-27 21:32:08 +0900

    feat: File Select Panel 을 Control File Panel로 기능 병합
    - Character Select Panel에서 뒤로가기 시 Control File Panel과 File Select Panel이 다른 Stack이기 때문에 분리되어 의도상 맞지 않아 병합

--- Assets/Min/Scripts/UIs/PanelBehaviours/ControlFilePanelButtons.cs
+++ Assets/Min/Scripts/UIs/PanelBehaviours/ControlFilePanelButtons.cs
@@ -12,19 +12,43 @@ namespace ProjectVS.UIs.PanelBehaviours.ControlFilePanelButtons
         public void OnClickNewButton()
         {
             //UIManager.Instance.Hide("Control File Panel");
-            UIManager.Instance.Show("File Select Panel");
+            //UIManager.Instance.Show("File Select Panel");
+
+            Debug.Log($"[ControlFilePanelButtons] New Button 호출");
         }
 
         public void OnClickLoadButton()
         {
             //UIManager.Instance.Hide("Control File Panel");
-            UIManager.Instance.Show("File Select Panel");
+            //UIManager.Instance.Show("File Select Panel");
+
+            Debug.Log($"[ControlFilePanelButtons] Load Button 호출");
         }
 
         public void OnClickDeleteButton()
         {
             //UIManager.Instance.Hide("Control File Panel");
-            UIManager.Instance.Show("File Select Panel");
+            //UIManager.Instance.Show("File Select Panel");
+
+            Debug.Log($"[ControlFilePanelButtons] Delete Button 호출");
+        }
+
+        public void OnClickFile1Button()
+        {
+            UIManager.Instance.Hide("Control File Panel");
+            UIManager.Instance.Show("Character Select Panel");
+        }
+
+        public void OnClickFile2Button()
+        {
+            UIManager.Instance.Hide("Control File Panel");
+            UIManager.Instance.Show("Character Select Panel");
+        }
+
+        public void OnClickFile3Button()
+        {
+            UIManager.Instance.Hide("Control File Panel");
+            UIManager.Instance.Show("Character Select Panel");
         }
 
         public void OnClickESCButton()
