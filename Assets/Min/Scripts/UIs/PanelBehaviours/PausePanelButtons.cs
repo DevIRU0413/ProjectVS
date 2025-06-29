44576a92ac615b998a6a050a62f8fbd1deb660e9
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-29 16:34:26 +0900

    fix: 현재 미구현 기능 주석으로 변경

--- Assets/Min/Scripts/UIs/PanelBehaviours/PausePanelButtons.cs
+++ Assets/Min/Scripts/UIs/PanelBehaviours/PausePanelButtons.cs
@@ -28,14 +28,18 @@ namespace ProjectVS.UIs.PanelBehaviours.CharacterIndicatorPausePanelButtons
 
         public void OnClickSettingsButton()
         {
-            UIManager.Instance.Hide("Pause Buttons Panel");
-            UIManager.Instance.Show("Settings Panel");
+            //UIManager.Instance.Hide("Pause Buttons Panel");
+            //UIManager.Instance.Show("Settings Panel");
+
+            Debug.Log($"[PausePanelButtons] Settings Button 패널 호출");
         }
 
         public void OnClickRecipeButton()
         {
-            UIManager.Instance.Hide("Pause Buttons Panel");
-            UIManager.Instance.Show("Recipe Panel");
+            //UIManager.Instance.Hide("Pause Buttons Panel");
+            //UIManager.Instance.Show("Recipe Panel");
+
+            Debug.Log($"[PausePanelButtons] Recipe Button 패널 호출");
         }
 
         public void OnClickGoToMainMenuButton()
