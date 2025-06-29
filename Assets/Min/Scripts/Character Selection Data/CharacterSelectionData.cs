8f9c13c1ce08a04c7a51e999f034ef63e269620c
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-29 15:09:38 +0900

    feat: 선택창 텍스트 표기용 CharacterSelectionData 클래스 추가
    - ID, Name 등의 프로퍼티를 통해 정보 접근 및 텍스트 활용 가능

---
+++ Assets/Min/Scripts/Character Selection Data/CharacterSelectionData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProjectVS.CharacterSelectionData.CharacterSelectionData
+{
+    [System.Serializable]
+    public class CharacterSelectionData
+    {
+        public int ID;
+        public string Name;
+        public int UniqueItemID;
+        public string ItemName // 아이템 고유 번호 CSV를 어떻게 활용했는지 아직 몰라서 프로퍼티로 구현함
+        {
+            get
+            {
+                switch (UniqueItemID)
+                {
+                    case 10001: return "검";
+                    case 10002: return "도끼";
+                    case 10003: return "지팡이";
+                    default: return "N/A";
+                }
+            }
+        }
+        public int Attack;
+        public int Defense;
+        public int HP;
+        public int AttackSpeed;
+        public int MoveSpeed;
+        public string IllustPath;
+        public string FlavorText;
+    }
+}
