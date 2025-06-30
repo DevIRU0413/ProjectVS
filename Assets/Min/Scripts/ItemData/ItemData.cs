81292b3f27cc74cf8eb5194a38dc6d05195a2291
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 22:04:19 +0900

    feat: 아이템 데이터 일부를 string에서 enum으로 받을 수 있도록 변경

--- Assets/Min/Scripts/ItemData/ItemData.cs
+++ Assets/Min/Scripts/ItemData/ItemData.cs
@@ -1,23 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 
 namespace ProjectVS.ItemData.ItemData
 {
+    public enum ItemRank
+    {
+        Sub = 0,
+        Anvil = 1,
+        Unique = 2,
+        Composite = 3
+    }
+
+    public enum ItemType
+    {
+        Money = 0,
+        Enforce = 1,
+        Potion = 2,
+        Attack = 3,
+        Passive = 4
+    }
+
+    public enum ItemEffect
+    {
+        None = 0,
+        Restore = 1,
+        Swing = 2,
+        Shot = 3,
+        RandomShot = 4,
+        Spin = 5,
+        HPStatUP = 6,
+        DefStatUP = 7,
+        SpeedStatUP = 8,
+        ThreeShot = 9,
+        Tornado = 10,
+        Throw = 11,
+        Fiveway = 12,
+        Bomb = 13,
+        Dobule = 14
+    }
+
+    public enum ItemSetEffect
+    {
+        TheftSet = 0,
+        MagicianSet = 1,
+        RobinSet = 2,
+        NinjaSet = 3,
+        KnightSet = 4
+    }
+
     [System.Serializable]
     public class ItemData
     {
         public int ID;
-        public string ItemRank;
+        public ItemRank ItemRank;
         public string ItemName;
-        public string ItemType;
+        public ItemType ItemType;
         public float AttackSpeed;
-        public string Effect;
+        public ItemEffect Effect;
         public float EffectValue;
         public int CombineItem1;
         public int CombineItem2;
-        public string SetEffect;
+        public ItemSetEffect SetEffect;
         public int SetItemCount;
         public int MaxLevel;
         public int Price;
