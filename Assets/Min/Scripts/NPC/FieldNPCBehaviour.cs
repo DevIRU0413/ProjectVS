82b9f7481c26b8529bf0c6a5d0f9ba2477bffa13
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 21:38:16 +0900

    refactor: 클래스 이름 변경
    - 상점 씬의 NPC와 필드의 NPC를 구분하기 위해 클래스 이름 변경

--- Assets/Min/Scripts/NPC/FieldNPCBehaviour.cs
+++ Assets/Min/Scripts/NPC/FieldNPCBehaviour.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 
 namespace ProjectVS.NPC.NPCBehaviour
 {
-    public class NPCBehaviour : MonoBehaviour
+    public class FieldNPCBehaviour : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer _spriteRenderer;
 
