75e175fdbabf2058b35647330bb72af7468e58c0
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 16:09:49 +0900

    feat: 세이브용 플레이어 데이터에 NPC 관련 필드 추가

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -17,15 +17,12 @@ namespace ProjectVS.Data
         public int                            Gold;                     // 소지 골드 (강화, 상점 구매 등 재화)
         public int                            Diamonds;                 // 소지 다이아 (프리미엄 재화, 재화 획득 시 계정 정보에 저장 필요)
 
-        // Help Me... R.I.P(수정하고 지워 주세요)
-        public List<AffectionData>           affectionLevels;          // 각 NPC 별 호감도 수치 및 경험치 (레벨 및 경험치 포함)
-        public List<string>                  affectionEventFlags;      // 이미 본 호감도 이벤트 플래그 (중복 발생 방지용)
+        public HashSet<int>                   ReadDialogeIDs;            // 플레이어가 이미 읽은 대사 ID 목록 (중복 방지용)
 
-        public List<string>                  dialogueProgressFlags;    // 대사 진행 플래그 (호감도 이벤트 흐름 제어)
+        public int                            CurrentAffinityExp;        // 현재 NPC 호감도 경험치
+        public int                            CurrentAffinityLevel;      // 현재 NPC 호감도 레벨
 
-        public List<string>                  ownedCostumes;            // 플레이어가 보유한 코스튬 목록 (재구매 방지 목적)
-        public List<NpcCostumeData>          npcCostumes;              // 각 NPC가 현재 착용 중인 코스튬 정보
-
-        public bool                          isMoodShifted;            // 플레이어 감정 변화 상태 여부 (특정 조건에서 모드 전환 여부 판단)
+        public HashSet<string>                AcquiredCostumeName;       // 플레이어가 획득한 코스튬 이름
+        public string                         WornCostumeName;           // NPC가 현재 착용 중인 코스튬 이름
     }
 }
