f02799fe933f17bbd666243c33858a6eabbddd99
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 15:05:25 +0900

    feat: 글리치 이펙트 로직 구현

---
+++ Assets/Min/Scripts/UIs/CutScene Effect/GlitchEffector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Kino;
+
+using UnityEngine;
+
+namespace ProjectVS.UIs.CutSceneEffect.GlitchEffector
+{
+    public class GlitchEffector : MonoBehaviour
+    {
+        [Header("글리치 이펙트 설정")]
+        [SerializeField] private DigitalGlitch _digitalGlitch;
+        [SerializeField] private float _glitchEffectDuration = 2f;
+
+        public void OnClickGlitchEffectImage()
+        {
+            StartCoroutine(IE_GlitchEffect());
+        }
+
+        private IEnumerator IE_GlitchEffect()
+        {
+            _digitalGlitch.intensity = 0.5f;
+
+            yield return new WaitForSeconds(_glitchEffectDuration);
+
+            _digitalGlitch.intensity = 0f;
+        }
+    }
+}
+
+
