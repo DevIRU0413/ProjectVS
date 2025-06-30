3e18013f23f02fbe546f237c6249cfe6fc5d4391
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 15:07:27 +0900

    feat: 컷씬의 활성화 초기화 로직 구현
    - 각 이미지를 활성화하는 방식으로 구현하였기에 다시 컷씬을 볼 때 활성화 순서를 초기화시켜주도록 로직 구현

---
+++ Assets/Min/Scripts/UIs/CutScene Effect/CutSceneInitiator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProjectVS.UIs.CutSceneEffect.CutSceneInitiator
+{
+    public class CutSceneInitiator : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _cutSceneObjects = new();
+        private List<int> _enabledList = new();
+
+        private void Awake()
+        {
+            if (_cutSceneObjects.Count == 0)
+            {
+                Debug.LogWarning("[CutSceneInitiator] 이미지 리스트가 비어있습니다");
+            }
+
+            foreach (var image in _cutSceneObjects)
+            {
+                _enabledList.Add(image.activeSelf ? 1 : 0);
+            }
+        }
+
+        private void OnDisable()
+        {
+            for (int i = 0; i < _cutSceneObjects.Count; i++)
+            {
+                _cutSceneObjects[i].SetActive(_enabledList[i] == 1);
+            }
+        }
+    }
+}
