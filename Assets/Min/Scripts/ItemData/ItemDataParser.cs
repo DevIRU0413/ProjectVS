7022d6a469ec7d0ac73b067c2ab668b743580338
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 22:05:11 +0900

    refactor: Enum으로 일부 변경한 프로퍼티에 의한 파싱 방법 수정

--- Assets/Min/Scripts/ItemData/ItemDataParser.cs
+++ Assets/Min/Scripts/ItemData/ItemDataParser.cs
@@ -6,6 +6,7 @@ using ProjectVS.Utils.CsvReader;
 using ItemDataClass = ProjectVS.ItemData.ItemData.ItemData;
 using ProjectVS.Utils.CsvTable;
 using ProjectVS.Utils.CsvParseUtils;
+using ProjectVS.ItemData.ItemData;
 
 
 namespace ProjectVS.ItemData.ItemDataParser
@@ -24,15 +25,15 @@ namespace ProjectVS.ItemData.ItemDataParser
                 ItemDataClass data = new()
                 {
                     ID = CsvParseUtils.TryParseInt(table.GetData(r, 1)),
-                    ItemRank = CsvParseUtils.TryParseString(table.GetData(r, 2)),
+                    ItemRank = (ItemRank)CsvParseUtils.TryParseInt(table.GetData(r, 2)),
                     ItemName = CsvParseUtils.TryParseString(table.GetData(r, 3)),
-                    ItemType = CsvParseUtils.TryParseString(table.GetData(r, 4)),
+                    ItemType = (ItemType)CsvParseUtils.TryParseInt(table.GetData(r, 4)),
                     AttackSpeed = CsvParseUtils.TryParseFloat(table.GetData(r, 5)),
-                    Effect = CsvParseUtils.TryParseString(table.GetData(r, 6)),
+                    Effect = (ItemEffect)CsvParseUtils.TryParseInt(table.GetData(r, 6)),
                     EffectValue = CsvParseUtils.TryParseFloat(table.GetData(r, 7)),
                     CombineItem1 = CsvParseUtils.TryParseInt(table.GetData(r, 8)),
                     CombineItem2 = CsvParseUtils.TryParseInt(table.GetData(r, 9)),
-                    SetEffect = CsvParseUtils.TryParseString(table.GetData(r, 10)),
+                    SetEffect = (ItemSetEffect)CsvParseUtils.TryParseInt(table.GetData(r, 10)),
                     SetItemCount = CsvParseUtils.TryParseInt(table.GetData(r, 11)),
                     MaxLevel = CsvParseUtils.TryParseInt(table.GetData(r, 12)),
                     Price = CsvParseUtils.TryParseInt(table.GetData(r, 13)),
