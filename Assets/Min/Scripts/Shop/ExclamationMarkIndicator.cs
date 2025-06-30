99bb2d4b24558a04557d6bb5385794c0909b62ee
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 17:05:51 +0900

    feat: 느낌표 출력 시스템 구현
    - 진행할 수 있는 이벤트가 있을 때 느낌표를 출력할 수 있는 기능 추가

---
+++ Assets/Min/Scripts/Shop/ExclamationMarkIndicator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using Unity.VisualScripting;
+
+using UnityEngine;
+
+using DialogueManagerClass = ProjectVS.Dialogue.DialogueManager.DialogueManager;
+
+namespace ProjectVS.Shop.ExclamationMarkIndicator
+{
+    public class ExclamationMarkIndicator : MonoBehaviour
+    {
+        [SerializeField] private DialogueManagerClass _dialogueManager;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            if (_spriteRenderer == null)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+        }
+
+
+        private void OnEnable()
+        {
+            CheckCanShowMark();
+        }
+
+        public void CheckCanShowMark()
+        {
+            if (_dialogueManager.CanShowEventDialogue())
+                _spriteRenderer.enabled = true;
+            else
+                _spriteRenderer.enabled = false;
+        }
+    }
+}
