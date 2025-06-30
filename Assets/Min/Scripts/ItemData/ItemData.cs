d8d9322e84bf0d80c7f8ec38156569f14efe1b1c
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 21:36:51 +0900

    feat: 아이템 데이터 관리용 클래스 추가
    - 각 프로퍼티를 활용하여 정보 접근 가능

---
+++ Assets/Min/Scripts/ItemData/ItemData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProjectVS.ItemData.ItemData
+{
+    [System.Serializable]
+    public class ItemData
+    {
+        public int ID;
+        public string ItemRank;
+        public string ItemName;
+        public string ItemType;
+        public float AttackSpeed;
+        public string Effect;
+        public float EffectValue;
+        public int CombineItem1;
+        public int CombineItem2;
+        public string SetEffect;
+        public int SetItemCount;
+        public int MaxLevel;
+        public int Price;
+        public float Range;
+        public float Size;
+        public string Description;
+        //public string ItemIconPath; TODO: 아이템 아이콘 경로(Resource폴더에 넣을) 추가해야 됨
+    }
+}
