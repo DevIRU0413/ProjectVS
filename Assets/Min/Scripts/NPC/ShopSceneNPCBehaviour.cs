2ca6df79201f1857ef18e5fd1445526d5a1e0712
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-30 21:38:55 +0900

    feat: 코스튬에 맞는 상점 주인 애니메이션 재생 시스템 구현
    - 현재 세부 로직 구현 중

---
+++ Assets/Min/Scripts/NPC/ShopSceneNPCBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CostumeStateManagerClass = ProjectVS.CharacterImages.CostumeStateManager.CostumeStateManager;
+using CostumeSOClass = ProjectVS.CharacterImages.CostumeSO.CostumeSO;
+
+
+namespace ProjectVS.NPC.ShopSceneNPCBehaviour
+{
+    public class ShopSceneNPCBehaviour : MonoBehaviour
+    {
+        [SerializeField] private CostumeStateManagerClass _costumeStateManager;
+
+        private Dictionary<string, Animator> _costumeAnimations;
+
+
+        private void Awake()
+        {
+            if (_costumeStateManager == null)
+            {
+                _costumeStateManager = FindObjectOfType<CostumeStateManagerClass>();
+            }
+
+            _costumeAnimations = new();
+
+            foreach (var anim in _costumeAnimations)
+            {
+                if (!_costumeAnimations.ContainsKey(anim.Key))
+                {
+                    _costumeAnimations.Add(anim.Key, anim.Value);
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            RenewCostumeAnimation();
+        }
+
+        // 옷을 입거나 벗었을 때에도 호출
+        private void RenewCostumeAnimation()
+        {
+            foreach (var costume in _costumeStateManager.CostumeSOs)
+            {
+
+            }
+        }
+    }
+}
