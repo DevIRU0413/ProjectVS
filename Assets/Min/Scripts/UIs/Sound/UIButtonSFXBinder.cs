160456e1d67296ff2c6c2ef238e74b388fb91d47
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-01 23:05:39 +0900

    feat: 버튼 클릭 시 사운드 재생 기능 추가

---
+++ Assets/Min/Scripts/UIs/Sound/UIButtonSFXBinder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using ProjectVS.Manager;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace ProjectVS.Uis.Sound.ButtonSoundMaker
+{
+    public class UIButtonSFXBinder : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _clickSound;
+        [SerializeField] private Button _button;
+
+
+        private void Awake()
+        {
+            if (_clickSound == null)
+                Debug.LogWarning("[UIButtonSFXBinder] AudioClip이 없습니다");
+
+            if (_button == null)
+                _button = GetComponent<Button>();
+            if (_button != null)
+                _button.onClick.AddListener(PlayClickSound);
+        }
+
+        private void PlayClickSound()
+        {
+            AudioManager.Instance.PlaySfx(_clickSound);
+        }
+    }
+}
