193deaf2265940aeb4ccbf9f9d450e1320d4aabf
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-02 23:03:13 +0900

    feat: SceneBase를 상속한 각 씬용 클래스 추가

---
+++ Assets/Min/Scripts/Scene/MainMenuScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProjectVS.Scene.MainMenuScene
+{
+    public class MainMenuScene : SceneBase
+    {
+        public override SceneID SceneID => SceneID.StoreScene;
+
+        protected override void Initialize()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Min/Scripts/Scene/StoreScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ProjectVS.Manager;
+
+using UnityEngine;
+
+
+namespace ProjectVS.Scene.MainMenuScene
+{
+    public class StoreScene : SceneBase
+    {
+        public override SceneID SceneID => SceneID.StoreScene;
+        public GameObject SpawnPoint;
+
+        protected override void Initialize()
+        {
+            SpawnPlayer();
+        }
+
+        private void SpawnPlayer()
+        {
+            Vector3 spawnPos = Vector3.zero;
+            if (SpawnPoint == null)
+            {
+                GameObject go = GameObject.FindGameObjectWithTag("PlayerSpawnPoint");
+                if (go != null)
+                    spawnPos = go.transform.position;
+            }
+            else
+                spawnPos = SpawnPoint.transform.position;
+
+            //var stats = PlayerDataManager.ForceInstance.stats;
+            //var classType = stats.CharacterClass;
+
+            //Unit.Player.PlayerSpawner.ForceInstance.SpawnPlayer(spawnPos, classType, stats);
+        }
+    }
+}
