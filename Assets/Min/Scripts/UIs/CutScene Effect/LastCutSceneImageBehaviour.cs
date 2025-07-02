37192228b1839f24c594b4f69fb05fce436a995c
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-02 11:14:48 +0900

    feat: CG 이후 비활성화 기능 추가

--- Assets/Min/Scripts/UIs/CutScene Effect/LastCutSceneImageBehaviour.cs
+++ Assets/Min/Scripts/UIs/CutScene Effect/LastCutSceneImageBehaviour.cs
@@ -7,25 +7,33 @@ namespace ProjectVS.UIs.CutSceneEffect.LastCutSceneImageBehaviour
 {
     public class LastCutSceneImageBehaviour : MonoBehaviour
     {
+        [SerializeField] private GameObject _openingScene;
+        [SerializeField] private GameObject _introScene;
+        [SerializeField] private GameObject _normalEndScene;
+        [SerializeField] private GameObject _trueEndScene;
 
         public void OnClickLastOpeningImage()
         {
             // 캐릭터 선택 끝나고
+            _openingScene.SetActive(false);
         }
 
         public void OnClickLastIntroImage()
         {
             // 인게임 씬 들어가자마자
+            _introScene.SetActive(false);
         }
 
         public void OnClickLastNormalEndImage()
         {
             // 노말엔딩 끝나고 호출해야될 것 여기서 호출
+            _normalEndScene.SetActive(false);
         }
 
         public void OnClickLastTrueEndImage()
         {
             // 트루엔딩 끝나고 호출해야될 것 여기서 호출
+            _trueEndScene.SetActive(false);
         }
     }
 }
