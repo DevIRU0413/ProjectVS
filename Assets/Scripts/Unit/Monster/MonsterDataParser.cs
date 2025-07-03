3b5daf870ba9f2991c04065b926c46df68b98595
DevIRU <deviru0413@gmail.com>
2025-07-03 10:41:43 +0900

    [style] 네임 스페이스 수정 및 간단 코드 수정

---
+++ Assets/Scripts/Unit/Monster/MonsterDataParser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProjectVS.Utils.CsvTable;
+using ProjectVS.Utils.CsvReader;
+using DialogueDataClass = ProjectVS.Dialogue.DialogueData.DialogueData;
+using ProjectVS.Utils.CsvParseUtils;
+
+namespace ProjectVS.Unit.Monster
+{
+    public static class MonsterDataParser
+    {
+        public static List<DialogueDataClass> Parse(CsvTable table)
+        {
+            CsvReader.Read(table);
+            List<DialogueDataClass> list = new();
+
+            int rowCount = table.Table.GetLength(0);
+
+            for (int r = 3; r < rowCount; r++)
+            {
+                DialogueDataClass data = new()
+                {
+                    ID = CsvParseUtils.TryParseInt(table.GetData(r, 0)),
+                    CharacterID = CsvParseUtils.TryParseInt(table.GetData(r, 1)),
+                    NeedAffinity = CsvParseUtils.TryParseInt(table.GetData(r, 2)),
+                    IllustPath = CsvParseUtils.TryParseString(table.GetData(r, 3)),
+                    Content = CsvParseUtils.TryParseString(table.GetData(r, 4)),
+                    ContextNote = CsvParseUtils.TryParseString(table.GetData(r, 6)),
+                    OccurTiming = CsvParseUtils.TryParseInt(table.GetData(r, 7))
+                };
+
+                list.Add(data);
+            }
+
+            return list;
+        }
+    }
+}
