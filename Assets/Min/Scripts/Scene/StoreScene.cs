fa79457fa7a42774bcefc641ec99343c48bbe4f0
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-03 18:20:39 +0900

    feat: 씬 진입 시 DialogueManagerR 클래스의 DialogueTextTyper 관련 필드 갱신 기능 추가

--- Assets/Min/Scripts/Scene/StoreScene.cs
+++ Assets/Min/Scripts/Scene/StoreScene.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+using ProjectVS.Dialogue.DialogueManagerR;
+using ProjectVS.Dialogue.TextEffect.DialogueTextTyper;
 using ProjectVS.Manager;
+
+using TMPro;
 
 using UnityEngine;
 
 
-namespace ProjectVS.Scene.MainMenuScene
+namespace ProjectVS.Scene.StoreScene
 {
     public class StoreScene : SceneBase
     {
         public override SceneID SceneID => SceneID.StoreScene;
         public GameObject SpawnPoint;
 
+        [SerializeField] private DialogueTextTyper _repeatText;
+        [SerializeField] private DialogueTextTyper _eventText;
+
         protected override void Initialize()
         {
             SpawnPlayer();
+            AssignText();
         }
 
         private void SpawnPlayer()
@@ -33,6 +42,14 @@ namespace ProjectVS.Scene.MainMenuScene
             //var classType = stats.CharacterClass;
 
             //Unit.Player.PlayerSpawner.ForceInstance.SpawnPlayer(spawnPos, classType, stats);
+        }
+
+        private void AssignText()
+        {
+            if (_repeatText == null) Debug.LogWarning($"[StoreScene] RepeatText가 등록되지 않았습니다");
+            if (_eventText == null) Debug.LogWarning($"[StoreScene] EventText가 등록되지 않았습니다");
+
+            DialogueManagerR.Instance.AssignTextWhenSceneChanged(_eventText, _repeatText, _eventText, _eventText);
         }
     }
 }
