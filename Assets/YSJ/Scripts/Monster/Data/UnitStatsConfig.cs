a8a534446ed26296f25a6e52b436c92f2c0e2954
DevIRU <deviru0413@gmail.com>
2025-07-03 10:35:05 +0900

    [fix] 유닛 이동 속도 관련 적용 안되는 버그 수정
    - UnitStats 매개변수 잘 못 기입으로 인한 버그 수정

--- Assets/YSJ/Scripts/Monster/Data/UnitStatsConfig.cs
+++ Assets/YSJ/Scripts/Monster/Data/UnitStatsConfig.cs
@@ -1,13 +1,25 @@
-﻿using UnityEngine;
+﻿using ProjectVS.Manager;
+
+using UnityEngine;
 
 namespace ProjectVS.Monster.Data
 {
     public class UnitStatsConfig : MonoBehaviour
     {
+        [field: Header("ID")]
+        [field: SerializeField] public int ID { get; private set; }
+
+        [field: Header("Stats")]
         [field: SerializeField] public float Hp { get; private set; }
         [field: SerializeField] public float ATK { get; private set; }
         [field: SerializeField] public float DFS { get; private set; }
         [field: SerializeField] public float SPD { get; private set; }
         [field: SerializeField] public float ATKSPD { get; private set; }
+
+        protected virtual void Awake()
+        {
+            // if (GameManager.Instance.GamePlayType == GamePlayType.Test) return;
+            // if (ID == 0) return;
+        }
     }
 }
