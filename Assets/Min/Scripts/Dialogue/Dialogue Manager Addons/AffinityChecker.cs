78314cc1447c1915643e0c8adac09969b8cbcba5
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-03 18:18:55 +0900

    feat: 호감도 조건 검사 전용 클래스 추가

---
+++ Assets/Min/Scripts/Dialogue/Dialogue Manager Addons/AffinityChecker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProjectVS.Dialogue.DialogueData;
+using ProjectVS.Shop.NPCAffinityModel;
+
+
+public class AffinityChecker
+{
+    public bool CanShow(DialogueData data)
+    {
+        return NPCAffinityModel.Instance.AffinityLevel >= data.NeedAffinity;
+    }
+}
