a463a5be527871012b366eaaca1303ef91292e7a
TaeHeon Ryoo <taroni55@naver.com>
2025-07-03 16:05:48 +0900

    [Feat] 아이템 이펙트 테스트용 스크립트 생성

---
+++ Assets/Scripts/Item/Test_Projectile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using ProjectVS.Interface;
+using ProjectVS.JDW;
+
+using UnityEngine;
+
+public class Test_Projectile : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+
+    private Vector2 _dir;
+    private float _damage;
+    private float _speed;
+
+    private void Awake()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(Vector2 dir, float dmg, float spd)
+    {
+        _dir = dir;
+        _damage = dmg;
+        _speed = spd;
+
+        Destroy(gameObject, 3f); // 3초 후 자동 제거
+    }
+
+    private void Update()
+    {
+        transform.Translate(_dir * _speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Monster"))
+        {
+            other.GetComponent<Monster>()?.TakeDamage(_damage);
+            Destroy(gameObject);
+        }
+    }
+}
