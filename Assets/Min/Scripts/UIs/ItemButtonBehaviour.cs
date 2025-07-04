3032ef720dc83abf17df16948cdc8f2546546315
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-04 15:37:16 +0900

    feat: 레시피 패널의 클릭 후 상세 정보로 접근 가능한 아이콘 버튼 로직 생성
    - 추후 Right Panel 에 접근하여 데이터를 수정하는 로직 추가 예정

---
+++ Assets/Min/Scripts/UIs/ItemButtonBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using ProjectVS.Item;
+using RecipePanelButtonsClass = ProjectVS.UIs.PanelBehaviours.RecipePanelButtons.RecipePanelButtons;
+
+
+namespace ProjectVS.UIs.ItemButtonBehaviour
+{
+    public class ItemButtonBehaviour : MonoBehaviour
+    {
+
+
+        [SerializeField] private Image _itemIcon;
+
+        private ItemData _data;
+        private RecipePanelButtonsClass _recipePanel;
+        private bool _isClickable;
+
+        public void Init(int itemID, RecipePanelButtonsClass recipePanel, bool isClickable)
+        {
+            _data = ItemDatabase.Instance.GetItem(itemID);
+            _recipePanel = recipePanel;
+            _isClickable = isClickable;
+
+            RenewAppearance();
+        }
+
+        private void RenewAppearance()
+        {
+            _itemIcon.sprite = _data.ItemIcon;
+        }
+
+        public void OnClickIconButton()
+        {
+            // TODO: _recipePanel의 필드에 있는 GameObject rightPanel 활성화 및 초기화
+        }
+    }
+}
