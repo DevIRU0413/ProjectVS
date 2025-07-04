c67b64d7c567ce67207c77ca12e70a84d794d4c1
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-04 15:38:47 +0900

    feat: 레시피 패널 버튼 메서드 기능 추가
    - 아이템, 세트 패널 토글 기능 추가

---
+++ Assets/Min/Scripts/UIs/PanelBehaviours/RecipePanelButtons.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectVS.UIs.PanelBehaviours.RecipePanelButtons
+{
+    public class RecipePanelButtons : MonoBehaviour
+    {
+        [SerializeField] private GameObject _itemPanel;
+        [SerializeField] private GameObject _setPanel;
+
+        private bool _isItemPanelEnabled = true;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void OnEnable()
+        {
+            Time.timeScale = 0f;
+
+            _isItemPanelEnabled = true;
+            CheckWhatPanelEnabled();
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1f;
+        }
+
+        public void OnClickItemButton()
+        {
+            _isItemPanelEnabled = true;
+            CheckWhatPanelEnabled();
+        }
+
+        public void OnClickSetButton()
+        {
+            _isItemPanelEnabled = false;
+            CheckWhatPanelEnabled();
+        }
+
+
+        public void CheckWhatPanelEnabled()
+        {
+            if (_isItemPanelEnabled)
+            {
+                _itemPanel.SetActive(true);
+                _setPanel.SetActive(false);
+            }
+        }
+    }
+}
