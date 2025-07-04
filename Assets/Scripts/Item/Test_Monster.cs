91c46e336febaba53b2340c7ae39d379074f1311
TaeHeon Ryoo <taroni55@naver.com>
2025-07-04 17:11:21 +0900

    [Feat] 데미지 팝업 구현

--- Assets/Scripts/Item/Test_Monster.cs
+++ Assets/Scripts/Item/Test_Monster.cs
@@ -1,7 +1,13 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+
+using TMPro;
+
+using UnityEngine;
 
 public class Test_Monster : MonoBehaviour
 {
+    [SerializeField] TMP_Text _damageText;
+
     public float maxHp = 100;
     public float currentHp;
 
@@ -12,12 +18,30 @@ public class Test_Monster : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        ShowDamageText(damage);
+
         currentHp -= damage;
         Debug.Log($"몬스터 피격 : {damage}, 남은 체력 : {currentHp}");
         if (currentHp <= 0)
         {
             Die();
         }
+    }
+
+    private void ShowDamageText(float damage)
+    {
+        _damageText.text = damage.ToString();
+        _damageText.alpha = 1f;
+        _damageText.rectTransform.anchoredPosition = Vector2.zero;
+
+        Sequence seq = DOTween.Sequence();
+        seq.Append(_damageText.rectTransform.DOAnchorPosY(50f, 0.5f)) // anchoredPosition 기준으로 위로 이동
+           .Join(_damageText.DOFade(0, 0.5f))
+           .OnComplete(() =>
+           {
+               _damageText.alpha = 0;
+               _damageText.rectTransform.anchoredPosition = Vector2.zero;
+           });
     }
 
     private void Die()
