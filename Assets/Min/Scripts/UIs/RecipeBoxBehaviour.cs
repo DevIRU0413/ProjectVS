d6c913fa1b686573e5b01f5ef2382762ea8811e3
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-04 15:40:07 +0900

    feat: 좌측의 레시피 박스 오브젝트 기능 추가
    - 각 조합아이템과 조합법에 해당하는 아이콘 생성
    - 해당 아이콘은 ItemButtonBehaviour를 통해 클릭이 가능하여 우측 상세 정보 확인 가능

---
+++ Assets/Min/Scripts/UIs/RecipeBoxBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using ProjectVS.Item;
+
+using UnityEngine;
+
+using RecipePanelButtonsClass = ProjectVS.UIs.PanelBehaviours.RecipePanelButtons.RecipePanelButtons;
+using ItemButtonBehaviourClass = ProjectVS.UIs.ItemButtonBehaviour.ItemButtonBehaviour;
+using TMPro;
+
+namespace ProjectVS.UIs.RecipeBoxBehaviour
+{
+    public class RecipeBoxBehaviour : MonoBehaviour
+    {
+        [SerializeField] private ItemButtonBehaviourClass _compositeItemButton;
+        [SerializeField] private ItemButtonBehaviourClass _baseItemButton1;
+        [SerializeField] private ItemButtonBehaviourClass _baseItemButton2;
+
+        [SerializeField] private TMP_Text _itemNameText;
+
+
+        private ItemData _item;
+        private RecipePanelButtonsClass _recipePanel;
+
+        public void Init(ItemData item, RecipePanelButtonsClass recipePanel)
+        {
+            _item = item;
+            _recipePanel = recipePanel;
+
+            RenewBoxAppearance();
+            CreateItemIconButtons();
+        }
+
+        private void RenewBoxAppearance()
+        {
+            _itemNameText.text = _item.ItemName;
+        }
+
+        private void CreateItemIconButtons()
+        {
+            _compositeItemButton.Init(_item.ItemID, _recipePanel, true);
+            _baseItemButton1.Init(_item.ItemAddID1, _recipePanel, true);
+            _baseItemButton2.Init(_item.ItemAddID2, _recipePanel, true);
+        }
+    }
+}
