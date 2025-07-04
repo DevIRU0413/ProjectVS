fc9fac51c92f35a556226efeade9fb71a1408e37
TaeHeon Ryoo <taroni55@naver.com>
2025-07-04 16:20:25 +0900

    [Fix] 아이템 Double 이펙트 로직 수정

---
+++ Assets/Scripts/Item/ItemEffect_Double.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using ProjectVS.JDW;
+
+using UnityEngine;
+
+public class ItemEffect_Double : MonoBehaviour
+{
+    private float _damage;
+
+    public void Init(float damage)
+    {
+        _damage = damage;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Monster"))
+        {
+            other.GetComponent<Monster>()?.TakeDamage(_damage);
+        }
+    }
+}
