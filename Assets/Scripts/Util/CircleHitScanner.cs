6cc3dada618891677613d2276555b49760655015
DevIRU <deviru0413@gmail.com>
2025-07-04 17:53:54 +0900

    [feat] 스캐너 기능 구현
    - 박스 스캐너  구현
    - 원 스캐너 구현
    - 히트 스캐너 인터페이스 구현

---
+++ Assets/Scripts/Util/CircleHitScanner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace ProjectVS.Util
+{
+    public class CircleHitScanner : MonoBehaviour, IHitScanner
+    {
+        public float radius = 1f;
+        public Vector2 offset = Vector2.zero;
+        public Collider2D[] buffer = new Collider2D[20];
+        public LayerMask targetMask;
+        public bool showGizimo = true;
+        public Color gizmoColor = new Color(0, 1, 0, 0.3f);
+
+        public GameObject[] Scan(Func<GameObject, bool> filter = null)
+        {
+            Vector2 center = (Vector2)transform.position + offset;
+            int count = Physics2D.OverlapCircleNonAlloc(center, radius, buffer, targetMask);
+
+            var list = new List<GameObject>();
+            for (int i = 0; i < count; i++)
+            {
+                var go = buffer[i].gameObject;
+                if (go != null && go.activeInHierarchy && (filter == null || filter(go)))
+                    list.Add(go);
+            }
+            return list.ToArray();
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (!showGizimo) return;
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawWireSphere((Vector2)transform.position + offset, radius);
+        }
+    }
+}
--- Assets/Scripts/Util/OverlapScanUtility.cs
+++ Assets/Scripts/Util/OverlapScanUtility.cs
@@ -7,18 +7,39 @@ namespace ProjectVS.Util
 {
     public static class OverlapScanUtility
     {
-        public static int SimpleScan(Vector2 center, float radius, LayerMask mask, Collider2D[] buffer)
+        public static int CircleScan(Vector2 center, float radius, LayerMask mask, Collider2D[] buffer)
         {
             int count = Physics2D.OverlapCircleNonAlloc(center, radius, buffer, mask);
             return count;
         }
 
-        public static List<GameObject> Scan(float radius, Vector2 center, LayerMask mask, Collider2D[] buffer, Func<GameObject, bool> filter = null)
+        public static int BoxScan(Vector2 center, Vector2 offset, Vector2 size, float angle, Collider2D[] buffer, LayerMask targetMask)
+        {
+            int count = Physics2D.OverlapBoxNonAlloc(center + offset, size, angle, buffer, targetMask);
+            return count;
+        }
+
+        public static List<GameObject> CircleScan(float radius, Vector2 center, LayerMask mask, Collider2D[] buffer, Func<GameObject, bool> filter = null)
         {
             var results = new List<GameObject>();
 
             int count = Physics2D.OverlapCircleNonAlloc(center, radius, buffer, mask);
 
+            for (int i = 0; i < count; i++)
+            {
+                var go = buffer[i].gameObject;
+                if (go == null || !go.activeInHierarchy) continue;
+                if (filter == null || filter(go)) results.Add(go);
+            }
+
+            return results;
+        }
+
+        public static List<GameObject> BoxScan(Vector2 center, Vector2 offset, Vector2 size, float angle, Collider2D[] buffer, LayerMask targetMask, Func<GameObject, bool> filter = null)
+        {
+            var results = new List<GameObject>();
+
+            int count = BoxScan(center, offset, size, angle, buffer, targetMask);
             for (int i = 0; i < count; i++)
             {
                 var go = buffer[i].gameObject;
