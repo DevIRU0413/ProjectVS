ecefe67f30e27ffdaaf40aa70df2939896421e86
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-04 15:41:08 +0900

    feat: 우측 상세정보 패널 공유 기능 구성
    - 각 세트, 레시피 패널이 동적으로 생성되기에 우측 상세 정보 패널 오브젝트를 참조할 수 있도록 해당 클래스 인스턴스를 외부에서 주입시켜주기 위한 기능 구성
    - 추후 로직 완성 예정

---
+++ Assets/Min/Scripts/UIs/RecipeBoxHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace ProjectVS.UIs.RecipeBoxHolder
+{
+    public class RecipeBoxHolder : MonoBehaviour
+    {
+        public GameObject _rightItemPanel;
+        public GameObject _rightSetPanel;
+
+        [SerializeField] private GameObject _leftRecipeBoxPrefab;
+        [SerializeField] private GameObject _leftSetBoxPrefab;
+
+
+
+
+
+
+
+    }
+}
