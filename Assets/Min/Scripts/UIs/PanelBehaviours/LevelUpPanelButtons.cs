35f02e0b4e2bd8ad5d82ac8a099537538413d879
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-04 12:00:02 +0900

    feat: 레벨업 아이템 선택 패널 버튼 기능 추가
    - 레벨업 이후 등장하는 UI 숨김 및 표시 기능 추가

---
+++ Assets/Min/Scripts/UIs/PanelBehaviours/LevelUpPanelButtons.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProjectVS.Utils.UIManager;
+using TMPro;
+
+
+namespace ProjectVS.UIs.PanelBehaviours.LevelUpPanelButtons
+{
+    public class LevelUpPanelButtons : MonoBehaviour
+    {
+        [SerializeField] private GameObject selectPanel;
+        [SerializeField] private TMP_Text _hideUIText;
+
+        private bool _isHiding = false;
+
+        private void OnEnable()
+        {
+            Time.timeScale = 0f;
+            _isHiding = false;
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1f;
+        }
+
+        public void OnToggleHideButton()
+        {
+            _isHiding = !_isHiding;
+
+            if (_isHiding)
+            {
+                selectPanel.SetActive(false);
+                _hideUIText.text = "UI 켜기";
+            }
+            else
+            {
+                selectPanel.SetActive(true);
+                _hideUIText.text = "UI 끄기";
+            }
+        }
+    }
+}
