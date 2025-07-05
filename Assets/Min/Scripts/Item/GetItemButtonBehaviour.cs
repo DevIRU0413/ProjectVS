68f51e0a04150f20d51d7ac3582bd79ef2c03659
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-06 01:12:18 +0900

    feat: 레벨업 아이템 버튼 기능 추가
    - OnClickGetButton에서 조합 기능 처리
    - 현재는 ItemCombinator에서 추가 메서드 처리 필요

--- Assets/Min/Scripts/Item/GetItemButtonBehaviour.cs
+++ Assets/Min/Scripts/Item/GetItemButtonBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 using ProjectVS.Utils.UIManager;
 
@@ -42,55 +43,106 @@ namespace ProjectVS.Item.GetItemButtonBehaviour
             _nameText.text = _itemData.ItemName;
 
             if ((int)_itemData.ItemEffect == 6) _descriptionText.text = $"체력 + {_itemData.ItemEffectValue}";
-            if ((int)_itemData.ItemEffect == 7) _descriptionText.text = $"방어력 + {_itemData.ItemEffectValue}";
-            if ((int)_itemData.ItemEffect == 8) _descriptionText.text = $"이동속도 + {_itemData.ItemEffectValue}";
+            else if ((int)_itemData.ItemEffect == 7) _descriptionText.text = $"방어력 + {_itemData.ItemEffectValue}";
+            else if ((int)_itemData.ItemEffect == 8) _descriptionText.text = $"이동속도 + {_itemData.ItemEffectValue}";
+            else _descriptionText.text = "";
         }
 
         public void OnClickGetButton()
         {
+            // 중복 선택 방지
             if (_isSelected) return;
 
-            // 조합 아이템인지 확인
-            if (_itemData.ItemRank == ItemRank.Composite)
+            // 현재 버튼이 조합 아이템이라면
+            if (_itemData.ItemRank == ItemRank.Sub)
             {
                 int id1 = _itemData.ItemAddID1;
                 int id2 = _itemData.ItemAddID2;
 
-                // 두 재료 아이템이 인벤토리에 있는지 확인
-                if (_itemInventory.HasItem(id1) && _itemInventory.HasItem(id2))
-                {
-                    // 둘 다 Max 레벨인지 확인
-                    //if (_itemInventory.최대레벨인지(id1) && _itemInventory.최대레벨인지(id2))
-                    //{
-                        // 조합 시도
-                        if (_itemCombinator.TryCombine(id1, id2, out ItemData result))
-                        {
-                            // 기존 아이템 제거
-                            _itemInventory.RemoveItem(ItemDatabase.Instance.GetItem(id1));
-                            _itemInventory.RemoveItem(ItemDatabase.Instance.GetItem(id2));
+                // 두 재료 아이템이 인벤토리에 존재하는지 확인
+                bool hasItem1 = _itemInventory.HasItem(id1);
+                bool hasItem2 = _itemInventory.HasItem(id2);
 
-                            // 조합 아이템 추가
-                            _itemInventory.AddItem(result);
-                        }
-                        else
+                // 두 재료 아이템 모두 최대 레벨이며, 조합된 적이 없는지 확인
+                bool isMax1 = _itemInventory.GetItemsByID(id1).All(i => i.ItemCurLevel >= i.ItemMaxLevel && !i.IsComposited);
+                bool isMax2 = _itemInventory.GetItemsByID(id2).All(i => i.ItemCurLevel >= i.ItemMaxLevel && !i.IsComposited);
+
+                // -- 디버그 로그
+                var items1 = _itemInventory.GetItemsByID(id1);
+                var items2 = _itemInventory.GetItemsByID(id2);
+
+                // 각 아이템의 이름을 쉼표로 이어붙임
+                string itemNames1 = string.Join(", ", items1.Select(i => i.ItemName));
+                string itemNames2 = string.Join(", ", items2.Select(i => i.ItemName));
+
+                // 조건 로그 출력
+                Debug.Log($"[조건 체크] item1(id:{id1}): [{itemNames1}] hasItem1: {hasItem1}, isMax1: {isMax1}");
+                Debug.Log($"[조건 체크] item2(id:{id2}): [{itemNames2}] hasItem2: {hasItem2}, isMax2: {isMax2}");
+                // -- 디버그 로그
+
+                // 조합 조건이 모두 충족되었을 경우
+                if (hasItem1 && hasItem2 && isMax1 && isMax2)
+                {
+                    if (_itemCombinator.TryCombine(id1, id2, out ItemData result))
+                    {
+                        var itemList1 = _itemInventory.GetItemsByID(id1);
+                        var itemList2 = _itemInventory.GetItemsByID(id2);
+
+                        foreach (var i in itemList1)
                         {
-                            // Debug.LogWarning("[GetItemButtonBehaviour] 둘 다 재료이면서 최대레벨이지만 조합 실패");
-                            // _itemInventory.레벨업(_itemData);
-                            // 아니면 _itemData.현재레벨++;
+                            if (!i.IsComposited && i.ItemCurLevel >= i.ItemMaxLevel)
+                            {
+                                i.IsComposited = true;
+                                _itemInventory.RemoveItem(i);
+                                Debug.Log($"[OnClickGetButton] {i.ItemName}이 조합되어 삭제됨");
+                                break;
+                            }
                         }
-                    //}
+
+                        foreach (var i in itemList2)
+                        {
+                            if (!i.IsComposited && i.ItemCurLevel >= i.ItemMaxLevel)
+                            {
+                                i.IsComposited = true;
+                                _itemInventory.RemoveItem(i);
+                                Debug.Log($"[OnClickGetButton] {i.ItemName} 삭제됨");
+                                break;
+                            }
+                        }
+
+                        Debug.Log($"[OnClickGetButton] {result.ItemName} 추가됨");
+                        _itemInventory.AddItem(result);
+
+                        Debug.Log($"[인벤토리 상태]");
+                        foreach (var item in _itemInventory.GetAllItems())
+                            Debug.Log($"{item.ItemName} (LV: {item.ItemCurLevel}, ID: {item.ItemID})");
+                    }
+                    else
+                    {
+                        Debug.LogWarning("[OnClickGetButton] TryCombine 실패 - 조합 조건은 충족했으나 실패");
+                    }
                 }
                 else
                 {
-                    // 단순 레벨업
-                    // _itemInventory.레벨업(_itemData);
-                    // 아니면 _itemData.현재레벨++;
+                    Debug.Log("[OnClickGetButton] 조합실패해서 그냥 레벨업");
+                    _itemData.ItemLevelUp();
                 }
 
                 _isSelected = true;
+                UIManager.Instance.ForceCloseTopPanel();
             }
-
-            UIManager.Instance.ForceCloseTopPanel();
+            else if (_itemData.ItemRank == ItemRank.Unique)
+            {
+                Debug.Log("[OnClickGetButton] 유니크 아이템이라 그냥 레벨업");
+                _itemData.ItemLevelUp();
+                UIManager.Instance.ForceCloseTopPanel();
+            }
+            else
+            {
+                Debug.Log("[OnClickGetButton] 이미 조합된 아이템이라 그냥 레벨업");
+                _itemData.ItemLevelUp();
+                UIManager.Instance.ForceCloseTopPanel();
+            }
         }
     }
 }
