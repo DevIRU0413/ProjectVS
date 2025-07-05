60b0ca6cfcfd76100b2870e39fab1eb2331e5d07
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-06 01:20:38 +0900

    fix: ItemData 대신 SetItem를 참조하도록 변경

--- Assets/Min/Scripts/UIs/Item/SetBoxBehaviour.cs
+++ Assets/Min/Scripts/UIs/Item/SetBoxBehaviour.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-using RecipePanelButtonsClass = ProjectVS.UIs.PanelBehaviours.RecipePanelButtons.RecipePanelButtons;
 using ItemButtonBehaviourClass = ProjectVS.UIs.Item.ItemButtonBehaviour.ItemButtonBehaviour;
 using ProjectVS.Item;
+using ProjectVS.Item.SetItemData;
+using RecipeBoxHolderClass = ProjectVS.UIs.Item.RecipeBoxHolder.RecipeBoxHolder;
 
 
 namespace ProjectVS.UIs.Item.SetOrRecipeBoxBehaviour
@@ -13,45 +14,60 @@ namespace ProjectVS.UIs.Item.SetOrRecipeBoxBehaviour
     public class SetBoxBehaviour : MonoBehaviour
     {
         [SerializeField] private ItemButtonBehaviourClass _setItemButton;
-        [SerializeField] private ItemButtonBehaviourClass _baseItemButton1;
-        [SerializeField] private ItemButtonBehaviourClass _baseItemButton2;
-        [SerializeField] private ItemButtonBehaviourClass _baseItemButton3;
-        [SerializeField] private ItemButtonBehaviourClass _baseItemButton4;
-
-
+        [SerializeField] private ItemButtonBehaviourClass[] _baseItemButtons;
         [SerializeField] private TMP_Text _itemNameText;
 
+        private RecipeBoxHolderClass _boxHolder;
 
-        private ItemData _item;
-        private RecipePanelButtonsClass _recipePanel;
-
-        public void Init(ItemData item, RecipePanelButtonsClass recipePanel)
+        public void Init(SetItemData setItem, RecipeBoxHolderClass boxHolder)
         {
-            _item = item;
-            _recipePanel = recipePanel;
+            _boxHolder = boxHolder;
 
-            RenewBoxAppearance();
-            CreateItemIconButtons();
+            RenewBoxAppearance(setItem);
+            CreateItemIconButtons(setItem);
         }
 
-        private void RenewBoxAppearance()
+        private void RenewBoxAppearance(SetItemData setItem)
         {
-            _itemNameText.text = _item.ItemName;
+            _itemNameText.text = setItem.SetName;
         }
 
-        private void CreateItemIconButtons()
+        // 아이템 버튼 세팅
+        private void CreateItemIconButtons(SetItemData setItem)
         {
-            // MEMO: 아이템 데이터 테이블에는 세트 아이템은 없는 듯??
-            // 따로 매핑을 해야될 듯
+            for (int i = 0; i < _baseItemButtons.Length; i++)
+            {
+                if (i < setItem.SetItemID.Length)
+                {
+                    int itemId = setItem.SetItemID[i];
+                    ItemData item = ItemDatabase.Instance.GetItem(itemId);
 
-            //_setItemButton.Init(_item.ItemID, _recipePanel, true);
-            //_baseItemButton1.Init(_item.ItemAddID1, _recipePanel, true);
-            //_baseItemButton2.Init(_item.ItemAddID2, _recipePanel, true);
+                    if (item == null)
+                    {
+                        Debug.LogWarning($"[SetBoxBehaviour] 존재하지 않는 아이템 ID: {itemId}");
+                        continue;
+                    }
 
-            //if (_item.ItemSetNum >= 3)
-            //{
-            //    _baseItemButton3.Init()
-            //}
+                    // 유효한 아이템이면 버튼을 초기화
+                    _baseItemButtons[i].Init(item.ItemID, _boxHolder, true);
+                    _baseItemButtons[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    // 세트 구성 아이템 개수보다 버튼 수가 많을 경우, 남는 버튼은 비활성화
+                    _baseItemButtons[i].gameObject.SetActive(false);
+                }
+            }
+
+            // 세트 대표 버튼은 세트 자체 아이콘을 사용
+            if (setItem.SetIcon != null)
+            {
+                _setItemButton.InitWithSetData(setItem, _boxHolder, true);
+            }
+            else
+            {
+                Debug.LogWarning($"[SetBoxBehaviour] 세트 아이콘을 찾을 수 없음");
+            }
         }
     }
 }
