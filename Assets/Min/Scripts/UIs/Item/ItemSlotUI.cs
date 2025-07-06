830457f751f38fb388294cf3c3e69d369c99d1a7
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-06 18:36:03 +0900

    feat: UI 아이템 슬롯 클래스 추가

---
+++ Assets/Min/Scripts/UIs/Item/ItemSlotUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+namespace ProjectVS.UIs.Item.ItemSlotUI
+{
+    public class ItemSlotUI : MonoBehaviour
+    {
+        [SerializeField] private Image _icon;
+
+        
+
+    }
+}
