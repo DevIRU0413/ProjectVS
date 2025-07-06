f0e3085a8c52732efa67e88dca7f00a09581467b
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-06 18:35:50 +0900

    feat: 인벤토리 UI 매니지용 클래스 추가

---
+++ Assets/Min/Scripts/UIs/Item/InventoryUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ItemSlotUIClass = ProjectVS.UIs.Item.ItemSlotUI.ItemSlotUI;
+using BuyItemObjBehaviourClass = ProjectVS.Item.BuyItemObjBehaviour.BuyItemObjBehaviour;
+using GetItemButtonBehaviourClass = ProjectVS.Item.GetItemButtonBehaviour.GetItemButtonBehaviour;
+
+
+namespace ProjectVS.UIs.Item.InventoryUI
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        [SerializeField] private List<ItemSlotUIClass> _uiSlots = new();
+
+        [SerializeField] private List<BuyItemObjBehaviourClass> _buyObjList;
+        [SerializeField] private List<GetItemButtonBehaviourClass> _getButtonList;
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        private void RenewUISlots()
+        {
+
+        }
+    }
+}
