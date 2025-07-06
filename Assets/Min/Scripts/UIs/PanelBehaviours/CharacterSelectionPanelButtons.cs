0e67efd39cadc52898d9e87d4543121f1f8d9c12
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-06 21:06:51 +0900

    fix: 필드 호출 변경

--- Assets/Min/Scripts/UIs/PanelBehaviours/CharacterSelectionPanelButtons.cs
+++ Assets/Min/Scripts/UIs/PanelBehaviours/CharacterSelectionPanelButtons.cs
@@ -18,6 +18,25 @@ namespace ProjectVS.UIs.PanelBehaviours.CharacterSelectionPanelButtons
 
         public void OnClickSelectButton()
         {
+            switch (_controlFilePanel.CurrentFileIndex)
+            {
+                case 1:
+                    PlayerDataManager.Instance.Stats.CharacterClass = CharacterClass.Sword;
+                    PlayerDataManager.ForceInstance.SavePlayerData(_controlFilePanel.CurrentFileIndex);
+                    break;
+                case 2:
+                    PlayerDataManager.Instance.Stats.CharacterClass = CharacterClass.Axe;
+                    PlayerDataManager.ForceInstance.SavePlayerData(_controlFilePanel.CurrentFileIndex);
+                    break;
+                case 3:
+                    PlayerDataManager.Instance.Stats.CharacterClass = CharacterClass.Magic;
+                    PlayerDataManager.ForceInstance.SavePlayerData(_controlFilePanel.CurrentFileIndex);
+                    break;
+                default:
+                    Debug.LogWarning($"[CharacterSelectionPanelButtons] 유효하지 않은 파일 인덱스: {_controlFilePanel.CurrentFileIndex}");
+                    break;
+            }
+
             Debug.Log($"[CharacterSelectionPanelButtons] 캐릭터 선택, 인게임씬 호출");
             SceneLoader.Instance.LoadSceneAsync(SceneID.InGameScene);
         }
