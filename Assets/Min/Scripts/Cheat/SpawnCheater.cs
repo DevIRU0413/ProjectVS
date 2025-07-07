2fde5da55bf234c804845e0b742c54530c5507ea
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-08 04:11:38 +0900

    test: 테스트용 치트 기능 수정

--- Assets/Min/Scripts/Cheat/SpawnCheater.cs
+++ Assets/Min/Scripts/Cheat/SpawnCheater.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ProjectVS.Manager;
+using ProjectVS.Monster;
+
 using UnityEngine;
 
-using ProjectVS.Monster;
 
 public class SpawnCheater : MonoBehaviour
 {
-    [SerializeField] private MonsterSpawnController _spawnController;
+    [SerializeField] private GameObject _bossPrefab;
 
 
+    public void OnClickSpawnBoss()
+    {
+        // 여기에 보스 스폰 기능 부탁드립니다
+    }
+
+    public void OnClickPressurePattern()
+    {
+        // 여기에 압박 패턴 기능 부탁드립니다
+    }
+
+    public void OnClickDashPattern()
+    {
+        // 여기에 돌진 패턴 기능 부탁드립니다
+    }
 }
--- Assets/Min/Scripts/Cheat/StageCheater.cs
+++ Assets/Min/Scripts/Cheat/StageCheater.cs
@@ -12,20 +12,5 @@ namespace ProjectVS.Cheat.StageCheater
         {
             // 여기에 스테이지 클리어 기능 부탁드립니다
         }
-
-        public void OnClickSpawnBoss()
-        {
-            // 여기에 보스 생성 기능 부탁드립니다
-        }
-
-        public void OnClickPressurePattern()
-        {
-            // 여기에 압박 패턴 기능 부탁드립니다
-        }
-
-        public void OnClickDashPattern()
-        {
-            // 여기에 돌진 패턴 기능 부탁드립니다
-        }
     }
 }
