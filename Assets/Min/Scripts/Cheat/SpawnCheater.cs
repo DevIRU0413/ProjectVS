0a893aa2c147fb9dca7c9a5f60df0957b4447fae
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-08 02:59:42 +0900

    test: 테스트용 치트 클래스 추가

---
+++ Assets/Min/Scripts/Cheat/SpawnCheater.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ProjectVS.Monster;
+
+public class SpawnCheater : MonoBehaviour
+{
+    [SerializeField] private MonsterSpawnController _spawnController;
+
+
+}
---
+++ Assets/Min/Scripts/Cheat/StageCheater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+namespace ProjectVS.Cheat.StageCheater
+{
+    public class StageCheater : MonoBehaviour
+    {
+        public void OnClickStageClear()
+        {
+            // 여기에 스테이지 클리어 기능 부탁드립니다
+        }
+
+        public void OnClickSpawnBoss()
+        {
+            // 여기에 보스 생성 기능 부탁드립니다
+        }
+
+        public void OnClickPressurePattern()
+        {
+            // 여기에 압박 패턴 기능 부탁드립니다
+        }
+
+        public void OnClickDashPattern()
+        {
+            // 여기에 돌진 패턴 기능 부탁드립니다
+        }
+    }
+}
