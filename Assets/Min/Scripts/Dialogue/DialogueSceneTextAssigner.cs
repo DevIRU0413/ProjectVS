e2fa7dbc1b2fa9584828a455c26a7f881302c4c9
Seonggyu-Min <minsunggu705@gmail.com>
2025-07-08 00:01:45 +0900

    feat: 각 씬마다 DialogueManager에 텍스트 등록 기능 추가

---
+++ Assets/Min/Scripts/Dialogue/DialogueSceneTextAssigner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using ProjectVS.Dialogue.TextEffect.DialogueTextTyper;
+using DialogueManagerClass = ProjectVS.Dialogue.DialogueManager.DialogueManager;
+using UnityEngine;
+
+
+namespace ProjectVS.Dialogue.DialogueSceneTextAssigner
+{
+    public class DialogueSceneTextAssigner : MonoBehaviour
+    {
+        [SerializeField] private DialogueTextTyper _shopEnterText;
+        [SerializeField] private DialogueTextTyper _repeatText;
+        [SerializeField] private DialogueTextTyper _eventText;
+        [SerializeField] private DialogueTextTyper _stageClearText;
+
+        public void Start()
+        {
+            AssignText();
+        }
+
+        private void AssignText()
+        {
+            DialogueManagerClass.Instance.AssignTextWhenSceneChanged(_shopEnterText, _repeatText, _eventText, _stageClearText);
+        }
+    }
+}
