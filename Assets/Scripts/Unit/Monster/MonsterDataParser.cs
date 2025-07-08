99aee69ae92fd24815fce6911ae558ffd0b1dea6
DevIRU <deviru0413@gmail.com>
2025-07-08 10:37:04 +0900

    [fix] tvs에 맞게 Parser 수정

--- Assets/Scripts/Unit/Monster/MonsterDataParser.cs
+++ Assets/Scripts/Unit/Monster/MonsterDataParser.cs
@@ -1,34 +1,46 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 
-using ProjectVS.Utils.CsvTable;
-using ProjectVS.Utils.CsvReader;
-using DialogueDataClass = ProjectVS.Dialogue.DialogueData.DialogueData;
 using ProjectVS.Utils.CsvParseUtils;
+using ProjectVS.Utils.CsvReader;
+using ProjectVS.Utils.CsvTable;
 
 namespace ProjectVS.Unit.Monster
 {
     public static class MonsterDataParser
     {
-        public static List<DialogueDataClass> Parse(CsvTable table)
+        public static List<MonsterStatsConfig> Parse(CsvTable table)
         {
-            CsvReader.Read(table);
-            List<DialogueDataClass> list = new();
+            CsvReader.Read(table); // 필요한 경우만 호출
+
+            List<MonsterStatsConfig> list = new();
 
             int rowCount = table.Table.GetLength(0);
 
-            for (int r = 3; r < rowCount; r++)
+            for (int r = 4; r < rowCount; r++) // 헤더+타입 3줄 넘기고
             {
-                DialogueDataClass data = new()
+                MonsterClass parsedClass;
+                Enum.TryParse(CsvParseUtils.TryParseString(table.GetData(r, 6)), out parsedClass);
+                var data = new MonsterStatsConfig
                 {
-                    ID = CsvParseUtils.TryParseInt(table.GetData(r, 0)),
-                    CharacterID = CsvParseUtils.TryParseInt(table.GetData(r, 1)),
-                    NeedAffinity = CsvParseUtils.TryParseInt(table.GetData(r, 2)),
-                    IllustPath = CsvParseUtils.TryParseString(table.GetData(r, 3)),
-                    Content = CsvParseUtils.TryParseString(table.GetData(r, 4)),
-                    ContextNote = CsvParseUtils.TryParseString(table.GetData(r, 6)),
-                    OccurTiming = CsvParseUtils.TryParseInt(table.GetData(r, 7))
+                    ID = CsvParseUtils.TryParseInt(table.GetData(r, 1)),
+                    MonsterName = CsvParseUtils.TryParseString(table.GetData(r, 2)),
+                    SizeValue = CsvParseUtils.TryParseInt(table.GetData(r, 5)),
+                    Class = parsedClass,
+
+                    Hp = CsvParseUtils.TryParseInt(table.GetData(r, 9)),
+                    ATK = CsvParseUtils.TryParseInt(table.GetData(r, 10)),
+                    ATKSPD = CsvParseUtils.TryParseFloat(table.GetData(r, 11)),
+                    ATKRange = CsvParseUtils.TryParseFloat(table.GetData(r, 12)),
+
+                    SPD = CsvParseUtils.TryParseFloat(table.GetData(r, 14)),
+                    DFS = CsvParseUtils.TryParseInt(table.GetData(r, 15)),
+
+                    Exp = CsvParseUtils.TryParseInt(table.GetData(r, 18)),
+                    DropGold = CsvParseUtils.TryParseInt(table.GetData(r, 19)),
+                    DropGoldPer = CsvParseUtils.TryParseInt(table.GetData(r, 20)),
+                    DropDiamond = CsvParseUtils.TryParseInt(table.GetData(r, 21)),
+                    DropDiamondPer = CsvParseUtils.TryParseInt(table.GetData(r, 22)),
                 };
 
                 list.Add(data);
