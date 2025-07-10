6ae238b4686a21d1c03b87f051b23cd3b1f993e4
DevIRU <deviru0413@gmail.com>
2025-07-11 02:13:13 +0900

    [feat]: 유닛 상태 머신 추가
    - 생성 시 기본 상태(Idle, Move, Dead) 추가
    - 차후 Skill 상태 추가 할 예정.

---
+++ Assets/Scripts/Runtime/Unit/UnitStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using ProjectVS.Core.FSM;
+using ProjectVS.Runtime.Unit;
+using ProjectVS.Runtime.Unit.FSM;
+
+namespace ProjectVS.FSM
+{
+    public class UnitStateMachine : StateMachine<UnitStateType, UnitState>
+    {
+        public UnitStateMachine(BaseUnitController owner)
+        {
+            RegisterState(new IdleState(owner));
+            RegisterState(new MoveState(owner));
+            RegisterState(new DeadState(owner));
+        }
+    }
+}
