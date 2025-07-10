7905c5a38d45b156f0e90faa221962c0c6173781
DevIRU <deviru0413@gmail.com>
2025-07-10 15:29:53 +0900

    [feat] BuffData ScriptableObject
    - BuffData 추가
    - 스탯 변경(StatModifier)
    - 상태 효과(UnitStatusEffect)
    - 지속 시간 포함

---
+++ Assets/Scripts/Definitions/Buffs/BuffData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using ProjectPV.Core.FSM;
+using ProjectPV.Core.Stat;
+
+using UnityEngine;
+
+namespace ProjectPV.Definitions.Buff
+{
+    [CreateAssetMenu(menuName = "Game/Buff/BuffData")]
+    public class BuffData : ScriptableObject
+    {
+        public string id;
+        public string displayName;
+        public float duration; // 0이면 무한 지속
+
+        public List<StatModifier> modifiers = new();
+        public List<UnitStatusEffect> statusEffects = new();
+    }
+}
