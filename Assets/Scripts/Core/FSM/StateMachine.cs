caa790c63961523984e1f99133e01108d0e5578a
DevIRU <deviru0413@gmail.com>
2025-07-11 01:28:34 +0900

    [feat]: 상태 머신 공용으로 사용 가능하게 구조 설계 및 구현
    - 상태로 사용할 타입과 상태로 사용할 인터페이스로 구현된 상태를 넣어 구현하면 기본적인 상태머신 사용이 가능한 상태 머신 구현

---
+++ Assets/Scripts/Core/FSM/StateMachine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectVS.Core.FSM
+{
+    public class StateMachine<TStateType, TState>
+        where TStateType : struct, System.Enum
+        where TState : IState<TStateType>
+    {
+        private readonly Dictionary<TStateType, TState> _states = new();
+        private TState? _current;
+
+        public TState Current => _current;
+        public TStateType? CurrentStateType => _current?.StateType;
+
+        public void RegisterState(TState state)
+        {
+            if (state == null) return;
+            _states[state.StateType] = state;
+        }
+
+        public void ChangeState(TStateType targetStateType)
+        {
+            if (_current != null && _current.StateType.Equals(targetStateType))
+                return;
+
+            if (!_states.TryGetValue(targetStateType, out var nextState))
+                return;
+
+            if (_current != null && !_current.CanTransitionTo(targetStateType))
+                return;
+
+            _current?.Exit();
+            _current = nextState;
+            _current.Enter();
+        }
+
+        public void Tick(float deltaTime)
+        {
+            _current?.Tick(deltaTime);
+        }
+
+        public void Clear()
+        {
+            _states.Clear();
+            _current = default;
+        }
+    }
+}
