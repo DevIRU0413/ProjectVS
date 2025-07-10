e9b74ad0059701512cebca1aefacc88779939f74
DevIRU <deviru0413@gmail.com>
2025-07-10 15:33:22 +0900

    [feat]: UnitStatusEffect enum 추가
    - 임시 여러 상태 추가

---
+++ Assets/Scripts/Core/FSM/UnitStatusEffect.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectPV.Core.FSM
+{
+    // 일단 생각 나는거 및 인터넷 찾아서 다 적음.
+    public enum UnitStatusEffect
+    {
+        None,           // 기본 상태 (효과 없음)
+        Stunned,        // 행동 불가
+        Slowed,         // 이동 속도 감소
+        Rooted,         // 이동 불가
+        Silenced,       // 스킬 사용 불가
+        Burning,        // 지속 데미지
+        Poisoned,       // 지속 데미지 (중첩 가능)
+        Frozen,         // 완전 정지
+        Invincible,     // 무적
+        Haste,          // 공격 속도 증가
+        Shielded,       // 방어막
+        Berserk,        // 공격력 증가, 방어력 감소
+        Taunted,        // 특정 타겟 강제 공격
+        Feared,         // 무작위 이동
+        KnockedBack,    // 넉백 처리
+        Charmed         // 적을 아군처럼 행동
+    }
+}
