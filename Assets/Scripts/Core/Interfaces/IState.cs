68fd2508b7b37d6181fd0613e09528edfa9f6a30
DevIRU <deviru0413@gmail.com>
2025-07-11 01:26:30 +0900

    [feat] 기본 상태의 인터페이스 추가
    - 해당 구조로 상태를 추가하여 만들 틀인 인터페이스 추가

---
+++ Assets/Scripts/Core/Interfaces/IState.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectVS.Core.FSM
+{
+    public interface IState<TStateType> where TStateType : struct, System.Enum
+    {
+        TStateType StateType { get; }
+
+        void Enter();
+        void Tick(float deltaTime);
+        void Exit();
+
+        bool CanTransitionTo(TStateType targetState);
+    }
+}
