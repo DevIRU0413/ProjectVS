cb8f3803461e9e188b2573d36b650fd0c058b68c
DevIRU <deviru0413@gmail.com>
2025-07-10 15:31:27 +0900

    [feat] BuffInstance for runtime state
    - BuffInstance 클래스 추가
    - remainingTime, stack, Tick() 함수 구현
    - IsExpired 속성으로 만료 여부 판단

---
+++ Assets/Scripts/Core/Buff/BuffInstance.cs
@@ -0,0 +1,27 @@
+﻿using ProjectPV.Definitions.Buff;
+
+using UnityEngine;
+
+namespace ProjectPV.Core.Buff
+{
+    public class BuffInstance
+    {
+        public BuffData data;
+        public float remainingTime;
+        public string sourceId => data.id;
+
+        public BuffInstance(BuffData data)
+        {
+            this.data = data;
+            this.remainingTime = data.duration;
+        }
+
+        public void Tick(float deltaTime)
+        {
+            if (data.duration <= 0) return; // 무한 지속
+            remainingTime -= deltaTime;
+        }
+
+        public bool IsExpired => data.duration > 0 && remainingTime <= 0;
+    }
+}
