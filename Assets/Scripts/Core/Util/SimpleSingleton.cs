ec60807dfbd39d438030adfdedd9425e4598f982
DevIRU <deviru0413@gmail.com>
2025-07-13 00:00:33 +0900

    [feat] 싱글톤 유틸 기능 추가

---
+++ Assets/Scripts/Core/Util/SimpleSingleton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Scripts.Util
+{
+    public abstract class SimpleSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T m_instance;
+        [SerializeField, HideInInspector] private bool m_isDontDestroyOnLoad = true;
+
+        public static T Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    m_instance = FindObjectOfType<T>();
+                    if (m_instance == null)
+                    {
+#if UNITY_EDITOR
+                        Debug.LogWarning($"[SimpleSingleton] {typeof(T)} 인스턴스가 없어 에디터에서 자동 생성됨.");
+#endif
+                        GameObject go = new GameObject($"@{typeof(T)}");
+                        m_instance = go.AddComponent<T>();
+
+                        if ((m_instance as SimpleSingleton<T>).IsDontDestroyOnLoad)
+                            DontDestroyOnLoad(go);
+                    }
+                }
+                return m_instance;
+            }
+        }
+
+        protected virtual bool IsDontDestroyOnLoad => m_isDontDestroyOnLoad;
+
+        protected virtual void Awake()
+        {
+            if (m_instance == null)
+            {
+                m_instance = this as T;
+                if (IsDontDestroyOnLoad)
+                    DontDestroyOnLoad(gameObject);
+            }
+            else if (m_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (m_instance == this)
+            {
+                m_instance = null;
+            }
+        }
+    }
+}
