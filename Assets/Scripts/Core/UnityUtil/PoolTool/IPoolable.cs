00a8592ac0561382b94371ae0e884e15ad0956ac
DevIRU <deviru0413@gmail.com>
2025-07-16 14:31:59 +0900

    [feat] UnityUtil - Pool 관련 기능 추가
    - SpawnObjectLifeCycle 풀링 매니저가 있다거나 사용할 거면 풀링 되는 오브젝트의 라이프 사이클 처리 해주는 기능
    - PoolManager 풀링 관련 매니징
    > 풀을 생성
    > 스폰 하려는 오브젝트의 이름이 존재 시 풀에서 활성화 진행 하여 스폰
    > 키로 풀 존재 여부 확인
    
    - IPoolable 풀이 가능한 오브젝트 cs에 상속하여 사용합니다.
    
    - SimpleSingleton 간단한 싱글톤 기능을 상속으로 제공하는 .cs 구현 추가
    
    - UnityUtilEx 추가

---
+++ Assets/Scripts/Core/UnityUtil/PoolTool/IPoolable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectVS.Util
+{
+    public interface IPoolable
+    {
+        Action OnSpawn { get; set; }
+        Action OnDespawn { get; set; }
+
+        void OnSpawned();
+        void OnDespawned();
+    }
+}
---
+++ Assets/Scripts/Core/UnityUtil/UnityUtilEx.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Util
+{
+    public static class UnityUtilEx
+    {
+        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
+        {
+            var comp = go.GetComponent<T>();
+            if (comp == null)
+                comp = go.AddComponent<T>();
+            return comp;
+        }
+
+        public static GameObject FindOrCreateGameObject(string name) 
+        {
+            GameObject go = GameObject.Find(name);
+            if (go == null)
+                go = new GameObject(name);
+            return go;
+        }
+
+        public static T FindNearestTarget<T>(Vector3 origin, List<GameObject> targets) where T : Component
+        {
+            float minDist = float.MaxValue;
+            T nearest = null;
+
+            foreach (var obj in targets)
+            {
+                if (obj == null) continue;
+
+                var target = obj.GetComponent<T>();
+                if (target == null) continue;
+
+                float dist = Vector2.Distance(origin, target.transform.position);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    nearest = target;
+                }
+            }
+
+            return nearest;
+        }
+    }
+}
